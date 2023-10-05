91d4188b1895a65e0bac198f25ce5f11b6a12bbc
Егоров Александр <pynyashkx@gmail.com>
2023-10-05 18:04:16 +0300

    Add ruined files

---
+++ Egorov_AA/1st_term/assignment_1/assignment_1.2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int func(FILE* input, FILE* output) {
+	double ans, mult = 1;
+	int n = 0;
+	while (!feof(input)) {
+		double temp;
+		if (fscanf(input, "%lf", &temp) != 1)
+			break;
+		mult = mult * temp;
+		n++;
+	}
+	if (n) {
+		ans = pow(mult, 1.0 / n);
+		fprintf(output, "%lf", ans);
+	}
+	else {
+		fprintf(output, "Input file is empty");
+		return -2;
+	}
+	return 0;
+}
+
+int main() {
+	char input_name[32], output_name[32];
+	scanf("%s%s", input_name, output_name);
+	FILE* input, * output;
+	input = fopen(input_name, "r");
+	output = fopen(output_name, "w");
+	if (input) {
+		int res = func(input, output);
+		if (res) {
+			return res;
+		}
+	}
+	else {
+		fprintf(output, "Failed to open input.txt");
+		return -1;
+	}
+	fclose(output);
+	fclose(input);
+	return 0;
+}
