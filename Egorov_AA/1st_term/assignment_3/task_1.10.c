7e1d92aa49d1c71b90a2088583566e40ff46044d
pynyashko <pynyashkx@gmail.com>
2023-10-13 23:48:11 +0300

    Add task 1.10

---
+++ Egorov_AA/1st_term/assignment_3/task_1.10.c
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int a_mean(FILE* input, int* error);
+
+int main(void) {
+	FILE* input, * output;
+	char input_name[32], output_name[32];
+	scanf("%s%s", input_name, output_name);
+	input = fopen(input_name, "r");
+	output = fopen(output_name, "w");
+	if (!input) {
+		printf("Failed to open input file\n");
+		return -1;
+	}
+	if (!output) {
+		printf("Failed to open output file\n");
+		return -2;
+	}
+	else {
+		int error;
+		int ans = a_mean(input, &error);
+		if (error == -1) {
+			fprintf(output, "Input file is empty");
+		}
+		else if (error == -2) {
+			fprintf(output, "Sequence is not neubivayuschaya");
+		}
+		else {
+			fprintf(output, "%d", ans);
+		}
+	}
+	fclose(input);
+	fclose(output);
+	return 0;
+}
+
+int a_mean(FILE* input, int* error) {
+	int count = 0, last_num, cur_num;
+	if (fscanf(input, "%d", &last_num) == 1) {
+		count++;
+	}
+	while (fscanf(input, "%d", &cur_num) == 1) {
+		if (cur_num > last_num) {
+			count++;
+			last_num = cur_num;
+		}
+		else if (cur_num < last_num) {
+			*error = -2; // error = -2 - последовательность НЕ неубывающая
+			return -2;
+		}
+	}
+	if (count) {
+		*error = 0; // error = 0 - все хорошо
+		return count;
+	}
+	else {
+		*error = -1; // error = -1 - пустой файл
+		return -1;
+	}
+}
