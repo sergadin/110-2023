56bbbc476f24731e4e4ecda4afc3e13e620ce5fb
monke de66uf <ppermakov85@gmail.com>
2023-10-15 15:57:52 +0300

    исправил 1.10

--- Permyakov_PA/1_10/task_1_10.c
+++ Permyakov_PA/1_10/task_1_10.c
@@ -9,21 +9,21 @@ int main(int argc, char** argv)
     int ans;
     if (argc != 2){
         printf("incorrect number of arguments. need only one - input filename\n");
-	return -1;
+	    return -1;
     }
     name = argv[1];
     ans = different_num_counter(name);
     if (ans == -2){
-	printf("can't open input file\n");
-	return 2;
+        printf("can't open input file\n");
+        return 2;
     }
     if (ans == -1){
     	printf("incorrect input data\n");
-	return 1;
+	    return 1;
     }
     if (ans == -3){
     	printf("incorrect input data - nondigit stuff\n");
-	return 3;
+	    return 3;
     }
     printf("The answer is %d\n", ans);
     return 0;
@@ -44,20 +44,20 @@ int different_num_counter(const char* name)
     	if (previous > present){
 	    fclose(fp);
 	    return -1;
-	}
-	if (previous != present){
-	    counter++;
-	    previous = present;
-        flag = 1;
-	}
-    if ((previous == present) & (flag == 1)){
-        counter++;
-        flag = 0;
-    }
+        }
+        if (previous != present){
+            counter++;
+            flag = 1;
+        }
+        if ((previous == present) & (flag == 1)){
+            counter++;
+            flag = 0;
+        }
+        previous = present;
     }
     if (!feof(fp)){
-	fclose(fp);
-	return -3;
+        fclose(fp);
+        return -3;
     }
     fclose(fp);
     return counter;
