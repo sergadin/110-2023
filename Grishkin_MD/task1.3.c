ec38f036b320adaf9bf08638f188ea9440876f17
Гришкин Михаил Денисович <Grishkin_MD@ns165.class.math.msu.su>
2023-11-11 12:33:09 +0300

    исправил задание 1.3

--- Grishkin_MD/task1.3.c
+++ Grishkin_MD/task1.3.c
@@ -3,26 +3,29 @@
 double calculateHarmonicMean(FILE* file);
 double mean; 
 int main(void) {
-    FILE* file = fopen("input.txt", "r");
+	FILE *foutput, *finput;
+    finput = fopen("input.txt", "r");
 
-    if (file == NULL) {
+    if (finput == NULL) {
         printf("Не удалось открыть файл.\n");
         return 1;
     }
-    mean = calculateHarmonicMean(file);
-    printf("Среднее гармоническое: %.2lf\n", mean);
+    mean = calculateHarmonicMean(finput);
+    foutput = fopen("output.txt", "w");
+    fprintf ( foutput, "Среднее гармоническое: %.2lf\n", mean);
 	    
-    fclose(file);
+    fclose(finput);
+    fclose(foutput);
 
     return 0;
 }
 
-double calculateHarmonicMean(FILE* file) {
+double calculateHarmonicMean(FILE* finput) {
     int number;
     int count = 0;
     double sum = 0.0;
 
-    while (fscanf(file, "%d", &number) == 1) {
+    while (fscanf(finput, "%d", &number) == 1) {
         sum += 1.0 / number;
         count++;
     }
