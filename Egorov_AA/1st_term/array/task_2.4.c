1436c19635c60368041549d29202523ba37de199
pynyashko <143870977+pynyashko@users.noreply.github.com>
2023-11-13 00:30:52 +0300

    update code

--- Egorov_AA/1st_term/array/task_2.4.c
+++ Egorov_AA/1st_term/array/task_2.4.c
@@ -2,17 +2,39 @@
 #include <stdlib.h>
 
 
-int comparasion(int* arr_1, int* arr_2, int* error);
+void comparasion(int* arr_a, int* arr_b, int len_a, int len_b);
 
 
-int comparasion(int* arr_1, int* arr_2, int* error) {
-    
+void comparasion(int* arr_a, int* arr_b, int len_a, int len_b) {
+	int count = 0;
+	for (int i = 0; i <= len_a; i++) {
+		for (int j = 0; j <= len_b; j++) {
+			if (arr_a[i] == arr_b[j]) {
+				count++;
+				break;
+			}
+		}
+	}
+	if (count == len_a) {
+		if (len_a == len_b) {
+			printf("A equals B\n");
+		}
+		else {
+			printf("A does not equal B\n");
+		}
+		printf("A is a subset of B\n");
+	}
+	else {
+		printf("A does not equal B\nA is not a subset of B\n");
+	}
 }
 
 int main(void) {
+
 	FILE* input_1, * input_2;
-	int* arr_1, * arr_2;
-	int len_1, len_2, error;
+	int* arr_a, * arr_b;
+	int len_a, len_b;
+
 	if ((input_1 = fopen("input_1.txt", "r")) == NULL) {
 		printf("Failed to open first input file\n");
 		return -1;
@@ -21,36 +43,39 @@ int main(void) {
 		printf("Failed to open second input file\n");
 		return -2;
 	}
-	if (!fscanf(input_1, "%d", &len_1)) {
+
+	if (!fscanf(input_1, "%d", &len_a)) {
 		printf("first input value error\n");
 		return -3;
 	}
-	if (!fscanf(input_2, "%d", &len_2)) {
+	if (!fscanf(input_2, "%d", &len_b)) {
 		printf("second input value error\n");
 		return -4;
 	}
-	arr_1 = (int*)malloc(len_1 * sizeof(int));
-	arr_2 = (int*)malloc(len_2 * sizeof(int));
-	if (arr_1 == NULL) {
-		printf("Memory allocation error");
+
+	arr_a = (int*)malloc(len_a * sizeof(int));
+	arr_b = (int*)malloc(len_b * sizeof(int));
+
+	if (arr_a == NULL) {
+		printf("Memory allocation error\n");
 		return -5;
 	}
-	if (arr_2 == NULL) {
-		printf("Memory allocation error");
+	if (arr_b == NULL) {
+		printf("Memory allocation error\n");
 		return -6;
 	}
-	for (int i = 0; i < len_1; i++) {
-        fscanf(input_1, "%d", &arr_1[i]);
-    }
-	for (int i = 0; i < len_2; i++) {
-        fscanf(input_2, "%d", &arr_2[i]);
-    }
-	comparasion(arr_1, arr_2, &error);
-    for (int i = 0; i < len_1; i++) {
-        printf("%d", arr_1[i]);
-    }
-    for (int i = 0; i < len_2; i++) {
-        printf("%d", arr_2[i]);
-    }
-    return 0;
+
+	for (int i = 0; i < len_a; i++) {
+		fscanf(input_1, "%d", &arr_a[i]);
+	}
+	for (int i = 0; i < len_b; i++) {
+		fscanf(input_2, "%d", &arr_b[i]);
+	}
+
+	comparasion(arr_a, arr_b, len_a, len_b);
+
+	free(arr_a);
+	free(arr_b);
+
+	return 0;
 }
