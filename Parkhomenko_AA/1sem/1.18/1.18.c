c4188d9d74c647283f1971ef6964079ed163b6b7
Parkhomenko Anastasia <anastasiia.parkhomenko@math.msu.ru>
2023-11-13 17:57:45 +0300

    изменила алгоритм задачи 1.18

--- Parkhomenko_AA/1sem/1.18/1.18.c
+++ Parkhomenko_AA/1sem/1.18/1.18.c
@@ -3,44 +3,43 @@
 #include <math.h>
 
 int OSHIBKA = 0;
+int NE_VOZR = 0;
+
+int opred_sum_chet_elem (FILE *f);
+
 
 int opred_sum_chet_elem (FILE *f)
 {
-    int tek, pred, sum = 0, elem = 0, vozr_posl = 1, nevozr_posl = 1, col_chet = 0;
-    //tek используется для хранения текущего считанного числа из файла
+    int tek, pred, sum = 0, schit_tek;
+//tek используется для хранения текущего считанного числа из файла
 //pred используется для хранения предыдущего считанного числа из файла
 //sum используется для хранения суммы четных чисел в последовательности
-//elem используется для подсчета количества считанных чисел из файла
-//vozr_posl и nevozr_posl используются для определения направления движения по последовательности (по возрастанию или убыванию)
-//col_chet используется для подсчета количества четных чисел в последовательности
  
- if (fscanf(f, "%d", &tek) != 1) {
-        OSHIBKA == -1;
-        return 0;
+    if (fscanf(f, "%d", &pred) != 1) {
+        OSHIBKA = -1;
+        return -1;
     }
 
-    while (fscanf(f, "%d", &tek) == 1) {
-        if (elem == 0) {
-            if (tek%2 == 0) sum += tek;
+    if (pred%2 == 0) sum += pred;
+
+    while ((schit_tek = fscanf(f, "%d", &tek)) != EOF) {
+        if (schit_tek != 1) {
+            OSHIBKA = -1;
+            return -1;
+        }
+        
+        if (tek > pred) {
+            if (tek%2 == 0) { sum += tek; }
         }
         else {
-            if (tek >= pred) {vozr_posl = 1;}
-            else {vozr_posl = -1;} 
-            if (tek%2 == 0 && vozr_posl == 1 && nevozr_posl == 1) { sum += tek; col_chet++; } 
-            if (vozr_posl == 1 && nevozr_posl == -1) {
-                if (pred%2 == 0) { sum += pred; col_chet++; }
-                if (tek%2 == 0) { sum += tek; col_chet++; }
-            } 
-            nevozr_posl = vozr_posl;
-            pred = tek;
+            NE_VOZR = 1;
+            return -1;
         }
-        elem++;
+        pred = tek;
     }
- 
-    if (col_chet == 0) {sum = 0;}
-    if (elem == 0) {sum = 1;}
     return sum;
 }
+
  
 int main(void) {
     int sum_ravna;
@@ -54,7 +53,10 @@ int main(void) {
     sum_ravna = opred_sum_chet_elem(f);
     
     if (OSHIBKA == -1) {
-		printf("Некорректный файл\n");
+		printf("Некорректный файл\n"); //пустой или есть буквы  
+    }
+    else if (NE_VOZR == 1) {
+		printf("Последовательность не возрастает\n");
     }
 	else {
 		printf("Сумма чётных элементов в возрастающей последовательности равна: %d\n", sum_ravna);
