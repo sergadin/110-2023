4e08c430632f2f2a953ed2eca2bb60f7cc4370fe
maria-popel <143882098+maria-popel@users.noreply.github.com>
2023-11-22 17:40:41 +0300

    Update z_7.c

--- Popel_MS/1sem/Sortings/z_7/z_7.c
+++ Popel_MS/1sem/Sortings/z_7/z_7.c
@@ -5,11 +5,11 @@
 #define Corr_ord 0
 #define Uncorr_ord -1
 
-void heap_sort(double *arr, int n);
+void heap_sort(double *arr, int len);
 int ordered_seq(double *arr, int len);
-int random_array(double *arr, int len_n);
+int random_array(double *arr, int len);
 void go_up(double *arr, int k);
-void go_down(double *arr, int n);
+void go_down(double *arr, int k);
 void swap(double *a, double *b);
 
 void swap(double *a, double *b){
@@ -31,18 +31,18 @@ void go_up(double *arr, int k){
 		k = j;
 	}		
 }
-void go_down(double *arr, int n){ 
+void go_down(double *arr, int k){ 
 	int i, i1, i2;
-	if (n < 2) {
+	if (k < 2) {
 		return;
 	}
-	for (i = 0; i < n; ) {
+	for (i = 0; i < k; ) {
 		i1 = 2 * i + 1;
 		i2 = i1 + 1;
-		if (i1 >= n) {
+		if (i1 >= k) {
 			break;
 		}
-		if ((i2 < n) && (arr[i1] < arr[i2])) {
+		if ((i2 < k) && (arr[i1] < arr[i2])) {
 			i1 = i2;
 		}
 		if (arr[i] < arr[i1]) {
@@ -53,12 +53,12 @@ void go_down(double *arr, int n){
 		}
 	}
 }
-void heap_sort(double *arr, int n){
+void heap_sort(double *arr, int len){
 	int k;
-	for (k = 1; k < n; k++){
+	for (k = 1; k < len; k++){
 		go_up(arr, k);
 	}
-	for (k = n - 1; k > 0; k--){
+	for (k = len - 1; k > 0; k--){
 		swap(arr, arr+k);
 		go_down(arr, k);
 	}
@@ -75,9 +75,9 @@ int ordered_seq(double *arr, int len){
 	return Corr_ord;
 }
 
-int random_array(double *arr, int len_n){
+int random_array(double *arr, int len){
 	srand(time(NULL));
-	for (int i = 0; i < len_n; i++){
+	for (int i = 0; i < len; i++){
 		arr[i] = rand()*(0.00001);
 	}
 	return 0;
@@ -89,7 +89,7 @@ int main(void){
 	clock_t start, end;
 	double *Array1 = NULL, *Array_rand = NULL, seconds;
 	FILE *f_in, *f_out; 
-	int len = 0, order1 = 0, order2 = 0, len_n, main_return_code = 0;
+	int len = 0, order1 = 0, order2 = 0, len_rand, main_return_code = 0;
 	
 	printf("Введите имя входного файла: \n");
 	if (scanf("%s" , input)!=1){
@@ -98,7 +98,7 @@ int main(void){
 	}
 	
 	printf("Введите длину случайного массива: \n");
-	if (scanf("%d", & len_n)!=1){
+	if (scanf("%d", & len_rand)!=1){
 		printf("Вы ввели некорректные значения\n");
 		return -1;
 	}
@@ -147,19 +147,19 @@ int main(void){
 	seconds = (double)(end-start);
 	order1 = ordered_seq(Array1, len);
 	
-	Array_rand = (double *)malloc(len_n*sizeof(double));
+	Array_rand = (double *)malloc(len_rand*sizeof(double));
 	if (Array_rand == NULL){
 		printf("Оперативная память не выделена\n");
 		goto terminate_1;
 	}
 	
-	if (random_array( Array_rand, len_n) == -1){
+	if (random_array( Array_rand, len_rand) == -1){
 		goto terminate_1;	
 	}
 	
-	heap_sort(Array_rand, len_n);
+	heap_sort(Array_rand, len_rand);
 	
-	order2 = ordered_seq(Array_rand, len_n);
+	order2 = ordered_seq(Array_rand, len_rand);
 	
 	
 	
@@ -174,7 +174,7 @@ int main(void){
 		fprintf(f_out, "%lf\n", Array1[i]);
 	}
 	fprintf(f_out, "%s\n", "\nRandom\n");
-	for (int i = 0; i < len_n; i++){
+	for (int i = 0; i < len_rand; i++){
 		fprintf(f_out, "%lf\n", Array_rand[i]);
 	}
 	
