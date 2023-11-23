e9581a2fa28f239a5d5bb2f64089f351cdd1e1b2
alinagelman <lina_art_raccon@mail.ru>
2023-11-23 20:36:36 +0300

    Задача 4
    Задание 11

--- Gelman_AD/Task4sem1.cpp
+++ Gelman_AD/Task4sem1.cpp
@@ -1,36 +1,32 @@
 #include<stdio.h>
 #include<<stdlib.h>>
 
-//здесь вывести принтф считывания К
-//считываем К
-printf("Enter the value of shifting the mas\n");
-fscanf(&K);
-
-while 
-//сюда запихнуть функцию сдвига массива
-
 //(определяю длину массива, считывая первый элемент в файле)
 
-void cyclic_Shift_Right(double* mas, int len);
-//получает на вход массив (mas) и его длину (len)
-//функция измененяет полученный на вход массив mas, циклически сдвигая каждый его элемент вправо
+void shiftrightk(double* mas, int len);
+//получает на вход массив (mas) и его длину (lngh)
+//функция циклически сдвигает полученный на вход массив mas, каждый элемент массива сдвигается вправо
 
 
-void cyclic_Shift_Right(double* mas, int len) {
+void shiftrightk(double* mas, int lngh)
+{
 	double posled_el;
 
-	if (len == 0) {
-		printf("net massiva, len = 0\n");
+	if (lngh == 0)
+	{
+		printf("No array, lngh = 0\n");
 		return;
 	}
 
-	if (len == 1) {     //нечего сдвигать, т к только один элемент
+	if (lngh == 1)
+	{     //нечего сдвигать, т к только один элемент
 		return;
 	}
 
-	posled_el = mas[len - 1];    //сохраняем последний элемент массива
+	posled_el = mas[lngh - 1];    //сохраняем последний элемент массива
 
-	for (int i = len - 1; i > 0; i--) {		//сдвигаем элементы массива вправо
+	for (int i = lngh - 1; i > 0; i--)
+	{		//сдвигаем элементы массива вправо
 		mas[i] = mas[i - 1];
 	}
 
@@ -38,42 +34,50 @@ void cyclic_Shift_Right(double* mas, int len) {
 }
 
 
-int main(void) {
-	int len; //длина массива
+int main(void) 
+{
+	int lngh; //длина массива
+	int K; //Будем сдвигать массив на К позиций вправо
 	FILE* file_in = fopen("input.txt", "r");
 	FILE* file_out = fopen("output.txt", "w");
 	double* mas = NULL;
 
-	if (file_in == NULL) {
-		printf("error opening the file_in\n");
+	if (file_in == NULL) 
+	{
+		printf("Unable to open the file input.txt\n");
 		return -1;
 	}
 
-	if (file_out == NULL) {
-		printf("error opening the file_out\n");
+	if (file_out == NULL) 
+	{
+		printf("Unable to open the file output.txt\n");
 		fclose(file_in);
 		return -1;
 	}
 
-	if (fscanf(file_in, "%d", &len) != 1) {
-		printf("error, v faile pusto / ne tot tip y dlinu\n");
+	if (fscanf(file_in, "%d", &lngh) != 1)
+	{
+		printf("Unable to read the file\n");
 		fclose(file_in);
 		fclose(file_out);
 		return -1;
 	}
 
-	mas = (double*)malloc((len) * sizeof(double));
+	mas = (double*)malloc((lngh) * sizeof(double));
 
-	if (mas == NULL) {
+	if (mas == NULL) 
+	{
 		printf("Memory error\n");
 		fclose(file_in);
 		fclose(file_out);
 		return -1;
 	}
 
-	for (int i = 0; i < len; i++) {
-		if (fscanf(file_in, "%lf", &mas[i]) != 1) {
-			printf("error, problema s tipom elementov massiva / dlina massiva < len\n");
+	for (int i = 0; i < lngh; i++)
+	{
+		if (fscanf(file_in, "%lf", &mas[i]) != 1) 
+		{
+			printf("Problems with array\n");
 			free(mas);
 			fclose(file_in);
 			fclose(file_out);
@@ -81,14 +85,26 @@ int main(void) {
 		}
 	}
 
-	cyclic_Shift_Right(mas, len);
+	//Считываем число К, введенное с клавиатуры
 
-	for (int i = 0; i < len; i++) {
+	printf("Enter the value of shifting the array\n");
+	fscanf(&K);
+
+	//Сдвигаем массив на К позиций вправо
+
+	for (int K = 0; K)
+	{
+		shiftrightk(mas, lngh);
+	}
+
+	for (int i = 0; i < lngh; i++)
+	{
 		fprintf(file_out, "%lf ", mas[i]);
 	}
 
-	if (len != 0) {
-		printf("rezultat napechatan\n");
+	if (lngh != 0)
+	{
+		printf("The result is printed\n");
 	}
 
 	free(mas);
