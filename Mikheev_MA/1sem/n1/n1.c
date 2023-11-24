9388b24524341db18e9d1b54a8e19d22723caa46
maxmiheev <makcmiheeb141@gmail.com>
2023-11-24 22:15:56 +0300

    Первый и второй номера
    Корректировка первой задачи и решение второй

--- Mikheev_MA/1sem/n1/n1.c
+++ Mikheev_MA/1sem/n1/n1.c
@@ -4,11 +4,10 @@
 #define success 0
 
 
-int last_num(FILE *input);
-int last_num(FILE *input)//функция определяет номер последнего числа, который равен минимуму последовательности
+int last_num(FILE *input, int* num);
+int last_num(FILE *input, int* num)//функция определяет номер последнего числа, который равен минимуму последовательности
 {
     int i=1;//счетчик чисел
-    int num=1;//номер конечного числа
     int rd;//переменная rd создана для проверки корректности данных файла
     double scan, min;
     rd = fscanf(input, "%lf", &min);
@@ -22,10 +21,10 @@ int last_num(FILE *input)//функция определяет номер пос
         i = i + 1;
         if(min >= scan){
             min = scan;
-            num = i;
+            *num = i;
         }
     }
-    return num;
+    return success;
 }
 
 
@@ -33,16 +32,17 @@ int main(void)
 {
    FILE *input;
     int res;
+    int num=1;//номер конечного числа
     input = fopen("input.txt", "r");
     if(input == NULL){
         printf("Не удалось открыть файл\n");
         return er_open;
     }
-    res = last_num(input);
+    res = last_num(input, &num);
     if(res == er_read){
         printf("Ошибка введенной последовательности\n");
         return er_read;
     }
-    printf("Номера числа %d \n", res);
+    printf("Номера числа %d \n", num);
     return success;
 }
