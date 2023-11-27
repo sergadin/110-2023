1d76ac5a67271535afe2872ee350cee2d4e37f79
Dima12Panov <dima.panov.2005@list.ru>
2023-11-27 19:45:35 +0300

    исправил задачу

--- Panov_DI/task5/task5.c
+++ Panov_DI/task5/task5.c
@@ -10,38 +10,52 @@ int function(FILE *input) {
     double epsilon = 0.001;
     int count = 0;
     int n = 0;
+
     if (fscanf(input, "%lf", &a) != 1) {
         printf("Не удается считать элемент\n");
 			return -1;
     }
+
     count++;
     if (fabs(x - a) <= epsilon) {
         n = count;
     }
+
     while (fscanf(input, "%lf", &a) == 1) {
         count++;
         if (fabs(x - a) <= epsilon) {
             n = count;
         }
-    }  
+    } 
+
+    if (feof (input) == 0){
+		printf("Файл не прочитался до конца\n");
+		return -1;
+	}
+
     return n;
 }
 
 int main() {
     int y;
     FILE *input;
-    input = fopen("/Users/dmitrij/Desktop/Учеба/test files/test5.txt", "r");
+
+    input = fopen("/Users/dmitrij/Desktop/Учеба/task5/test5.txt", "r");
     if(input == NULL)
-		{
-			printf("Не удается открыть файл\n");
-			return -1;
-		}
+	{
+		printf("Не удается открыть файл\n");
+		return -1;
+	}
+
     y = function(input);
+
     if (y == 0) {
         printf("В последовательности нет чисел равных X\n");
     } else if (y > 0) {
         printf("Номер последнего числа, равного X: %d\n", y);
     }
+
     fclose(input);
+
     return 0;
 }
