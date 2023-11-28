3979248c9fe603209960c4d963eb878b499f4281
Kisaeva_SA <ks1273@mail.ru>
2023-11-29 02:49:14 +0300

    создание функции и вынесение ответа в файл в задаче 4.15

--- Kisaeva_SA/1sem/4zad/4.15.c
+++ Kisaeva_SA/1sem/4zad/4.15.c
@@ -3,37 +3,54 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void) {
-	int N; // натуральное число, считываемое с клавиатуры
-	int izmen_N;
+void razl(FILE *file_out, int N);
+// функция разложения натурального числа на простые множители
+
+
+void razl(FILE *file_out, int N) {
+	int izmen_N = N;
 	int st; // степень простого множителя
 	
-	printf("vvedite chislo N = ");
-	
-	if ((scanf("%d", &N) != 1) || (N < 1)) {
-		printf("not korr N (not natur)\n");
-		return -1;
-	}
-	
-	printf("%d = 1", N);
-	
-	izmen_N = N;
+	fprintf(file_out, "%d = 1", N);
 	
 	for (int i = 2; i < pow(N, 0.5) + 1; i++) {
 		if ((izmen_N % i) == 0) {	
-			printf(" * ");
-			printf ("%d^", i);   // простой множитель
+			fprintf(file_out, " * ");
+			fprintf (file_out, "%d^", i);   // простой множитель
 			for (st = 0; (izmen_N % i) == 0; st++) {
 				izmen_N = izmen_N / i;
 			}
-			printf("%d", st);   // степень простого множителя
+			fprintf(file_out, "%d", st);   // степень простого множителя
 		}
 	}
 	
 	if (izmen_N > 1) {
-		printf(" * %d", izmen_N);
-	}	
+		fprintf(file_out, " * %d", izmen_N);
+	}
+}
+
+
+int main(void) {
+	int N; // натуральное число, считываемое с клавиатуры
+	FILE *file_out = fopen("output.txt", "w");
 	
-	printf("\n");
+	if (file_out == NULL) {
+		printf("error opening the file_out");
+		return -1;
+	}
+	
+	printf("vvedite natur chislo N = ");
+	
+	if ((scanf("%d", &N) != 1) || (N < 1)) {
+		printf("not korr N (not natur)\n");
+		fclose(file_out);
+		return -1;
+	}
+	
+	razl(file_out, N);	
+	
+	printf("rezultat napechatan\n");
+	
+	fclose(file_out);
 	return 0;
 }
