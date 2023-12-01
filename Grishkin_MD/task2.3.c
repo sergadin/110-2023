10c14a0f18da06d30c2b18133f398c251f14935e
MishaGrishkin15 <143871147+MishaGrishkin15@users.noreply.github.com>
2023-12-01 22:42:56 +0300

    Add files via upload

---
+++ Grishkin_MD/task2.3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void reverseArray(int arr[], int length);
+
+void reverseArray(int arr[], int length) {
+    int start = 0;
+    int end = length - 1;
+    int temp;
+
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+int main(void) {
+    int lenght;
+    int* arr;
+    FILE* file = fopen("input.txt", "r");
+    if (file == NULL) {
+        printf("ne udalos'ortrit' file \n");
+        return 1;
+    }
+    fscanf(file, "%d", &lenght);
+    arr = malloc(lenght * sizeof(int));
+
+    if (arr == NULL) {
+        printf("memory error\n");
+        fclose(file);
+        return -1;
+    }
+    for (int i = 0; i < lenght; i++) {
+        fscanf(file, "%d", &arr[i]);
+    }
+
+    fclose(file);
+
+    printf("array: ");
+    for (int i = 0; i < lenght; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    reverseArray(arr, lenght);
+
+    printf("Reversed array: ");
+    for (int i = 0; i < lenght; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    free(arr);
+    return 0;
+}
