1b0e764a47453020f9790a1403b2dfa45a69b692
pynyashko <143870977+pynyashko@users.noreply.github.com>
2023-12-07 22:37:07 +0300

    add code for task 4.5

--- Egorov_AA/1st_term/bit_operation/task_4.5.c
+++ Egorov_AA/1st_term/bit_operation/task_4.5.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void) {
-    
+void longToCharArray(long num, unsigned char* byteArray) {
+    for (int i = 0; i < sizeof(long); i++) {
+        byteArray[i] = (num >> (8 * (sizeof(long) - i - 1)));
+    }
+}
+
+long charArrayToLong(char* byteArray) {
+    long num = 0;
+    for (int i = 0; i < sizeof(long); i++) {
+        num += ((long)(byteArray[i]) << (8 * (sizeof(long) - i - 1)));
+    }
+    return num;
+}
+
+int main() {
+    long number = 29458723; // Пример числа
+    unsigned char byteArray[sizeof(long)];
+    longToCharArray(number, byteArray);
+    printf("long to char array: ");
+    for (int i = 0; i < sizeof(long); i++) {
+        printf("%c \n", byteArray[i]);
+    }
+    printf("\n");
+
+    long new_number = charArrayToLong(byteArray);
+    printf("char array to long: %ld\n", new_number);
+
+    return 0;
 }
