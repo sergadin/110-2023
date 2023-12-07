868167d0c7eec3b68446ccaff464b2f8abf54403
maria-popel <143882098+maria-popel@users.noreply.github.com>
2023-12-08 00:58:45 +0300

    Update z_7.c

--- Popel_MS/1sem/Geometry/z_7.c
+++ Popel_MS/1sem/Geometry/z_7.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-
+			//Необходимо найти рассточние между двумя многоугольниками, заданными координатами точек. Если мн-ки пересекаются - расстояние равно 0.
 typedef struct
 {
 	double x;
@@ -18,14 +18,14 @@ typedef struct
 
 double min(double a, double b);
 Point vector(Point a, Point b);
-void to_output(Point *coords, int len1, int len2, FILE *f_out1, FILE *f_out2, Point p_st, Point p_end);
-void to_gnuplot(FILE *gnuplt);
-double scalar_product(Point a, Point b, Point c, Point d);
-double oblique_product(Point a, Point b, Point c, Point d);
-Segm point_distance(Point a, Point b);
-Segm segment_point_distance(Point a, Point b, Point c);
-bool is_cross(Point p1, Point p2, Point m1, Point m2);
-Segm segment_distance(Point a, Point b, Point c, Point d);
+void to_output(Point *coords, int len1, int len2, FILE *f_out1, FILE *f_out2, Point p_st, Point p_end); 
+void to_gnuplot(FILE *gnuplt); //Загрузка координат многоугольника в файл для постройки графика
+double scalar_product(Point a, Point b, Point c, Point d); //скалярное произведение
+double oblique_product(Point a, Point b, Point c, Point d); //косое произведение
+Segm point_distance(Point a, Point b); //расстояние между точками
+Segm segment_point_distance(Point a, Point b, Point c); //расстояние между точкой и отрезком
+bool is_cross(Point p1, Point p2, Point m1, Point m2); //проверка пересечения многоугольников
+Segm segment_distance(Point a, Point b, Point c, Point d); //рассчет минимального расстояния
 
 double min(double a, double b){
 	if (a < b){
@@ -38,13 +38,13 @@ double min(double a, double b){
 Point vector(Point a, Point b){
 	Point ab;
 	
-	ab.x = (b.x-a.x);
+	ab.x = (b.x - a.x);
 	ab.y = (b.y - a.y);
 	return ab;
 }
 
 void to_output(Point *coords, int len1, int len2, FILE *f_out1, FILE *f_out2, Point p_st, Point p_end){
-	Point first1=coords[0], first2=coords[len1];
+	Point first1 = coords[0], first2 = coords[len1];
 	int len_m, count = 0;
 	
 	if (len1>len2){
@@ -53,12 +53,12 @@ void to_output(Point *coords, int len1, int len2, FILE *f_out1, FILE *f_out2, Po
 		len_m = len2;
 	}
 	
-	for (int i = 0; i<len_m; i++){
-		if (i<len1 && i<len2){
+	for (int i = 0; i < len_m; i++){
+		if (i < len1 && i < len2){
 			fprintf(f_out1, "%lf %lf %lf %lf\n", coords[i].x, coords[i].y, coords[len1+i].x, coords[len1+i].y);
-		}else if (i<len1 && i>=len2){
+		}else if (i < len1 && i >= len2){
 			fprintf(f_out1, "%lf %lf %lf %lf\n", coords[i].x, coords[i].y, first2.x, first2.y);
-		}else if (i>=len1 && i<len2){
+		}else if (i >= len1 && i < len2){
 			fprintf(f_out1, "%lf %lf %lf %lf\n", first1.x, first1.y, coords[len1+i].x, coords[len1+i].y);
 		}
 	}
@@ -77,7 +77,7 @@ double scalar_product(Point a, Point b, Point c, Point d){
 	
 	v1 = vector(a,b);
 	v2 = vector(c,d);
-	return(v1.x*v2.x + v1.y*v2.y);
+	return(v1.x * v2.x + v1.y * v2.y);
 
 }
 
@@ -86,14 +86,14 @@ double oblique_product(Point a, Point b, Point c, Point d){
 	
 	v1 = vector(a,b);
 	v2 = vector(c,d);
-	return(v1.x*v2.y - v1.y*v2.x);
+	return((v1.x * v2.y) - (v1.y * v2.x));
 }
 
 Segm point_distance(Point a, Point b){
 	Segm dist;
 	double x1 = a.x, y1 = a.y, x2 = b.x, y2 = b.y;
 	
-	dist.x = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+	dist.x = sqrt(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 	dist.y = a;
 	dist.z = b;
 	return dist;
@@ -105,14 +105,14 @@ Segm segment_point_distance(Point a, Point b, Point c){
 	double x0 = a.x, y0 = a.y, x1 = b.x, y1 = b.y, x2 = c.x, y2 = c.y;
 	double sq, length, param;
 	
-	sq = (x2-x1)*(y0-y1)-(y2-y1)*(x0-x1);
-	length = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	sq = (x2 - x1)*(y0 - y1)-(y2 - y1)*(x0 - x1);
+	length = sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1));
 	
 	if (scalar_product(b,a,b,c)>0 && scalar_product(c,a,c,b)>0){
 		dist.x = fabs(sq/length);
-		param = ((y2-y1)*(x1-x0)+(x1-x2)*(y1-y0))/((y2-y1)*(y2-y1)+(x1-x2)*(x1-x2));
-		h.x = x0+(y2-y1)*param;
-		h.y = y0+(x1-x2)*param;
+		param = ((y2 - y1)*(x1 - x0)+(x1 - x2)*(y1 - y0)) / ((y2 - y1)*(y2 - y1) + (x1 - x2)*(x1 - x2));
+		h.x = x0 + (y2 - y1)*param;
+		h.y = y0 + (x1 - x2)*param;
 		dist.y = a;
 		dist.z = h;
 		return (dist);
@@ -120,7 +120,7 @@ Segm segment_point_distance(Point a, Point b, Point c){
 	else{
 		dist1 = point_distance(a,b);
 		dist2 = point_distance(a,c);
-		if (dist1.x<dist2.x){
+		if (dist1.x < dist2.x){
 			return dist1;
 		}else{
 			return dist2;
@@ -132,19 +132,19 @@ bool is_cross(Point p1, Point p2, Point m1, Point m2){
 	double obl1, obl2, obl3, obl4, on_seg1, on_seg2, on_seg3, on_seg4;
 	int on_seg = 0;
 	
-	obl1 = oblique_product(p1,p2,p1,m2);
-	obl2 = oblique_product(p1,p2,p1,m1);
-	obl3 = oblique_product(m1,m2,m1,p1);
-	obl4 = oblique_product(m1,m2,m1,p2); 
-	on_seg1 = scalar_product(m2,p1,m2,p2);
-	on_seg2 = scalar_product(m1,p1,m1,p2);
-	on_seg3 = scalar_product(p1,m1,p1,m2);
-	on_seg4 = scalar_product(p2,m1,p2,m2);
+	obl1 = oblique_product(p1, p2, p1, m2);
+	obl2 = oblique_product(p1, p2, p1, m1);
+	obl3 = oblique_product(m1, m2, m1, p1);
+	obl4 = oblique_product(m1, m2, m1, p2); 
+	on_seg1 = scalar_product(m2, p1, m2, p2);
+	on_seg2 = scalar_product(m1, p1, m1, p2);
+	on_seg3 = scalar_product(p1, m1, p1, m2);
+	on_seg4 = scalar_product(p2, m1, p2, m2);
 	
-	if ((on_seg1 <=0 && obl1==0) || (on_seg2<=0 && obl2==0) || (on_seg3<=0 && obl3==0) || (on_seg4<=0 && obl4==0)){
+	if ((on_seg1 <= 0 && obl1 == 0) || (on_seg2 <= 0 && obl2 == 0) || (on_seg3 <= 0 && obl3 == 0) || (on_seg4 <= 0 && obl4 == 0)){
 		on_seg = 1;
 	}
-	if ((obl1*obl2<0 && obl3*obl4<0) || on_seg == 1){
+	if ((obl1*obl2 < 0 && obl3*obl4 < 0) || on_seg == 1){
 		return true;
 	}else{
 		return false;
@@ -153,7 +153,7 @@ bool is_cross(Point p1, Point p2, Point m1, Point m2){
 }
 
 Segm segment_distance(Point a, Point b, Point c, Point d){
-	double l1,l2,l3,l4;
+	double l1, l2, l3, l4;
 	Segm s1, s2, s3, s4;
 	
 	if (is_cross(a,b,c,d)){
@@ -162,23 +162,23 @@ Segm segment_distance(Point a, Point b, Point c, Point d){
 		s1.z = b;
 		return s1;
 	}
-	s1 = segment_point_distance(a,c,d);
-	s2 = segment_point_distance(b,c,d);
-	s3 = segment_point_distance(c,a,b);
-	s4 = segment_point_distance(d,a,b);
+	s1 = segment_point_distance(a, c, d);
+	s2 = segment_point_distance(b, c, d);
+	s3 = segment_point_distance(c, a, b);
+	s4 = segment_point_distance(d, a, b);
 	
 	l1 = s1.x;
 	l2 = s2.x;
 	l3 = s3.x;
 	l4 = s4.x;
 	
-	if (min(l1,l2)<min(l3,l4)){
-		if (l1<l2){
+	if (min(l1, l2)<min(l3, l4)){
+		if (l1 < l2){
 			return s1;
 		}else{
 			return s2;
 		}
-	}else if (l3<l4){
+	}else if (l3 < l4){
 		return s3;
 	}else{
 		return s4;
@@ -191,7 +191,7 @@ int main(void){
 	Point dist_st, dist_end;
 	Segm dist1, dist2;
 	int len1, len2;
-	double min_dist = 1000000000,thing = 0.;
+	double min_dist = 1000000000, thing = 0.;
 	
 	f_in = fopen("input.txt", "r");
 	if (f_in == NULL){
@@ -225,7 +225,7 @@ int main(void){
 	
 	fscanf(f_in, "%d%d", &len1, &len2);
 	
-	coords = (Point *)malloc((len1+len2)*sizeof(Point));
+	coords = (Point *)malloc((len1 + len2)*sizeof(Point));
 	
 	if (coords == NULL){
 		printf("Оперативная память не выделена\n");
@@ -241,9 +241,9 @@ int main(void){
 	dist_end.x= 0;
 	dist_end.y = 0;
 	
-	for (int i=0; i<len1; i++){
-		for (int j = 0; j<len2; j++){
-			dist1 = segment_distance(coords[i], coords[(i+1)%len1], coords[len1+j], coords[len1+(j+1)%len2]);
+	for (int i = 0; i < len1; i++){
+		for (int j = 0; j < len2; j++){
+			dist1 = segment_distance(coords[i], coords[(i + 1)%len1], coords[len1 + j], coords[len1 + (j + 1)%len2]);
 			if (min_dist>dist1.x){
 				min_dist = dist1.x;
 				dist_st = dist1.y;
@@ -252,7 +252,7 @@ int main(void){
 			if (min_dist==0.){
 				dist_st.x = 0;
 				dist_st.y = 0;
-				dist_end.x= 0;
+				dist_end.x = 0;
 				dist_end.y = 0;
 				printf("Многоугольники пересекаются\n");
 				goto terminate_1;
@@ -262,15 +262,18 @@ int main(void){
 	}
 	
 	printf("Расстояние между многоугольниками: - %lf\n", min_dist);
+	
 	terminate_1:
 	to_output(coords, len1, len2, f_out1, f_out2, dist_st, dist_end);
 	to_gnuplot(gnuplt);
 	free  (coords);
+	
 	terminate:
 	fclose(f_in);
 	fclose(f_out1);
 	fclose(f_out2);
 	fclose(gnuplt);
+	
 	return 0;
 
 }
