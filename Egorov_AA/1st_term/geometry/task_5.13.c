8f147bfefba9a6fdef8049fca59f07b66be90772
Егоров Александр <aleksandr.egorov@math.msu.ru>
2023-12-09 12:24:50 +0300

    edit code

--- Egorov_AA/1st_term/geometry/task_5.13.c
+++ Egorov_AA/1st_term/geometry/task_5.13.c
@@ -2,5 +2,33 @@
 
 
 int main(void){
-	asdlkajsdklajsdlkajsdlksajdlajdlkasjd
+	FILE* input;
+    int n;
+    int* x-axis, * y-axis;
+    if ((input = fopen("input.txt", "r") == NULL) {
+        printf("failed to open input file");
+        return -1;
+    }
+    
+    fscanf(input, "%d", &n);
+    
+    x-axis = (int*)malloc(n * sizeof(int));
+    if (x-axis == NULL){
+        printf("memory allocation error");
+        return -2;
+    }
+    
+    y-axis = (int*)malloc(n * sizeof(int));
+    if (y-axis == NULL){
+        printf("memory allocation error");
+        return -3;
+    }
+    
+    for (int i = 0; i < n; i++)
+        fscanf(input, "%d", &x-axis[i]);
+    for (int i = 0; i < n; i++)
+        fscanf(input, "%d", &y-axis[i]);
+    
+    
+    
 }
