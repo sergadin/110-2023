6a4fdead400375cef048582d5a8282774419799d
Averasok <144222903+Averasok@users.noreply.github.com>
2023-12-16 00:22:32 +0300

    Update 2_23.c

--- Kosareva_DT/1 sem/2_23/2_23.c
+++ Kosareva_DT/1 sem/2_23/2_23.c
@@ -1,137 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int addition(FILE *f, int n, char *str);
-int subtraction(FILE *f, int n, char *str);
+int addition(int *a, int n);
+int subtraction(int *a, int n);
 int main(void);
 
-int addition(FILE *f, int n, char *str) {
-	int check, a[n], i, one;
-
-	a[0] = 0;
-	for (i=1; i<n; i++) {
-		check = fscanf(f, "%d", &a[i]);
-		if (check != 1) {
-			printf("Ошибка данных\n");
-			return -2;
-		}
-		if (a[i] > 9) {
-			printf("Ошибка данных\n");
-                        return -2;
-		}
-	}
-	fclose(f);
-	
-	one = 1;
-	for (i=n-1; i>=0; i--) {
-		a[i] += one;
-		one = a[i]/10;
-		a[i] = a[i]%10;
-	}
-
-	f = fopen(str, "w");
-	one = 0;
-	for (i=0; i<n; i++) {
-		if (a[i] > 0) {
-			one = 1;
-		}
-		if (one == 1) {
-			fprintf(f, "%d ", a[i]);
-		}
-	}
-	return 0;
-}
-
-int subtraction(FILE *f, int n, char *str) {
-        int check, a[n], i, one;
-
-        a[0] = 1;
-        for (i=1; i<n; i++) {
-                check = fscanf(f, "%d", &a[i]);
-                if (check != 1) {
-                        printf("Ошибка данных\n");
-                        return -2;
-                }
-                if (a[i] > 9) {
-                        printf("Ошибка данных\n");
-                        return -2;
-                }
-        }
-        fclose(f);
-
-        one = 1;
-        for (i=n-1; i>=0; i--) {
-                a[i] += 10-one;
-                one = 1 - a[i]/10;
-                a[i] = a[i]%10;
-        }
-
-        f = fopen(str, "w");
-        one = 0;
-        for (i=1; i<n; i++) {
-                if (a[i] > 0) {
-                        one = 1;
-                }
-                if (one == 1) {
-                        fprintf(f, "%d ", a[i]);
-                }
-        }
-        return 0;
-}
-
 int main(void) {
-        int check, n, action;
+        int check, n, action, i;
         FILE *f;
-        char str[60];
-
-        check = scanf("%s", str);
-        if (check == 0) {
-                printf("Не указано имя массива\n");
-                return -2;
-        }
+        int *a = NULL;
+	
+	printf("1\n");
+	
+        check = scanf("%d", &n);
         if (check == -1) {
-                printf("Ошибка данных1\n");
+                printf("Не указана длина массива\n");
                 return -2;
         }
+        if (check == 0) {
+                printf("Ошибка данных\n");
+                return -2;
+        }
+        
+        printf("2\n");
 
-        f = fopen(str, "r");
+        f = fopen("a", "r");
         if (f == NULL) {
                 printf("Не удалось открыть файл\n");
                 return -1;
         }
-
-        check = fscanf(f, "%d", &n);
+        
+        printf("3\n");
+        
+	a = (int *)malloc((n+1)*sizeof(int));
+	for (i=1; i<n+1; i++) {
+                check = fscanf(f, "%1d", &a[i]);
+                if (check != 1) {
+                	free(a);
+                        printf("Ошибка данных\n");
+                        fclose(f);
+                        return -2;
+                }
+        }
+        
+        printf("4\n");
+        
+        fclose(f);
+        
+        check = scanf("%d", &action);
         if (check == -1) {
-                printf("Не указана длинна массива\n");
+                printf("Не указано действие\n");
+                free(a)
                 return -2;
         }
         if (check == 0) {
-                printf("Ошибка данных2\n");
+                printf("Ошибка данных\n");
+                free(a);
                 return -2;
         }
-
-	check = scanf("%d", &action);
-	if (check == 0) {
-                printf("Не указано действие\n");
-                return -2;
-        }
-        if (check == -1) {
-                printf("Ошибка данных1\n");
-                return -2;
-        }
-	
+        
 	if (action == 1) {
-		addition(f, n, str);
+		addition(a, n);
+	}
+	else if (action == -1) {
+		subtraction(a, n);
 	}
 	else {
-		if (action == -1) {
-			subtraction(f, n, str);
-		}
-		else {
-			printf("Ошибка данных\n");
-			return -2;
-		}
-	}
-
-        fclose(f);
+		printf("Ошибка данных\n");
+                return -2;
+        }
+        
+        f = fopen("a", "w");
+       	i = 0;
+       	while (a[i] == 0) {
+       		i++;
+       	}
+       	while (i<=n) {
+       		fprintf(f, "%d ", a[i]);
+       		i++;
+       	}
+       	free(a);
+       	fclose(f);
+       		
+        return 0;
 }
 
+int addition(int *a, int n) {
+	int i = n;
+	a[0] = 0;
+	
+	while (i>0) {
+		if (a[i] < 9) {
+			a[i]++;
+			i = 0;
+		}
+		else {
+			a[i] = 0;
+			i--;
+		}
+	}
+	
+	return 0;
+}
+
+int subtraction(int *a, int n) {
+        int i = n;
+      	a[0] = 1;
+      	
+      	while (i>0) {
+		if (a[i] > 0) {
+			a[i]--;
+			i = 0;
+		}
+		else {
+			a[i] = 0;
+			i--;
+		}
+	}
+	a[0] = 0;
+	
+	return 0;
+}
