f846c6b3b320ff5fbcad480c8070dfaed7d35149
Alina Gelman <lina_art_raccon@mail.ru>
2023-12-16 14:48:02 +0300

    Task 9

--- Gelman_AD/Task9sem1.cpp
+++ Gelman_AD/Task9sem1.cpp
@@ -2,76 +2,63 @@
 
 int check_frame(int icon[32])
 {
-    int i;
+    int correct = 0;
 
-    // Checking the upper border
-    for (i = 0; i < 32; i++) 
+    if (icon[0] == (2^32) - 1)
     {
-        if (icon[i] != 1) 
-        {
-            return 0; // There is no black frame on the icon
-        }
+        correct = 1; // Checking the upper border
     }
 
-    // Checking the lower border
-    for (i = 31; i < 32; i--) 
+    if (icon[31] == (2^32) - 1)
     {
-        if (icon[i] != 1) 
-        {
-            return 0; // There is no black frame on the icon
-        }
+        correct = 1; // Checking the lower border
     }
 
-    // Checking the left border
-    for (i = 0; i < 32; i += 8) 
+    if (icon[1] == (2^31) + 1)
     {
-        if (icon[i] != 1) 
-        {
-            return 0; // There is no black frame on the icon
-        }
+        correct = 1; //Left black line
     }
 
-    // Checking the right border
-    for (i = 7; i < 32; i += 8) 
-    {
-        if (icon[i] != 1) 
-        {
-            return 0; // There is no black frame on the icon
-        }
-    }
+    if (icon[30] == (2^31) + 1)
+   {
+        correct = 1; //Right black line
+   }
 
-    // Checking pixels inside the frame
-    for (i = 1; i < 31; i++) 
-    {
-        for (int j = 1; j < 7; j++) 
-        {
-            if (icon[i * 8 + j] != 0) 
-            {
-                return 0; // There is no black frame on the icon
-            }
+   for (int i = 1; i < 31; i++) //No black lines inside the frame
+   {
+       for (int j = 1; j < 7; j ++)
+       {
+           if (icon[i * 8 + j] != 0)
+           {
+               correct = 0;
+           }
         }
-    }
+   }
+     
 
-    return 1; //There is a black frame
+    return correct; //There is a black frame
 }
 
 
 int main() 
 {
     int icon[32];
+    int correct;
 
     printf("Enter 32 integers of 0 and 1\n");
 
     for (int i = 0; i < 32; i++) 
     {
-        scanf("%u", &icon[i]);
+        scanf("%d", &icon[i]);
     }
 
-    if (check_frame(icon)) //return = 1
+    correct = check_frame(icon);
+
+    if (correct == 1)
     {
         printf("There is a black frame\n");
     }
-    else //return = 0
+    else //correct == 0
     {
         printf("There is no black frame on the icon\n");
     }
