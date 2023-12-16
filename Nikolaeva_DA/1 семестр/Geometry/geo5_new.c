8d92585f7c203af44a15da5c6d67444fac7c8f4d
Nikolaeva_DA <darina.nikolaeva@math.msu.ru>
2023-12-16 19:18:08 +0300

    Новая версия задачи
    Теперь я учла тот факт, что нужно все-таки от центра отсчитывать.

--- Nikolaeva_DA/1 семестр/Geometry/geo5_new.c
+++ Nikolaeva_DA/1 семестр/Geometry/geo5_new.c
@@ -7,72 +7,48 @@
 
 
 struct Point{
-        double x;
-        double y;
+	double x;
+	double y;
 };
-
-double distance(struct Point p1, struct Point p2);
 double minDim(struct Point polygon[], int n);
+double distance(struct Point p1, struct Point p0);
 double maxDim(struct Point polygon[], int n);
 
- /* unsigned int coren(unsigned int a)
-{
-   unsigned int  x;
-   x = (a/0x3f + 0x3f)>>1;
-   x = (a/x + x)>>1;
-   x = (a/x + x)>>1;
-   return(x); 
-}
-unsigned int isqrt(unsigned int x)
-{
-   unsigned int m, y, b;
-   m = 0x4000;
-   y = 0;
-   while (m != 0){
-      b = y | m;
-      y = y >> 1;
-      if (x >= b) {
-         x = x - b;
-         y = y | m;
-      }
-      m = m >> 2;
-   }
-   return y;
-}*/
 
-//Функция считает расстояние между 2 точками
-double distance(struct Point p1, struct Point p2){
-	double dx = p2.x - p1.x;
-	double dy = p2.y - p1.y;
+double distance(struct Point p1, struct Point p0){
+	double dx = p0.x - p1.x;
+	double dy = p0.y - p1.y;
 	return  sqrt(dx * dx + dy * dy);
 }
-//Функция находит минимальный диаметр(Находим расстояние между 2 точками, находя самую удалённую
 
 
-double minDim(struct Point polygon[], int n){
-	struct Point A = polygon[0];
-	for(int i = 1; i < n; i++){
-		if(polygon[i].x < A.x){
-			A = polygon[i];
-		}
+double minDim(struct Point polygon[], int n) {
+	// Находим координаты центра многоугольника
+	double sumX = 0, sumY = 0;
+	for (int i = 0; i < n; i++) {
+		sumX += polygon[i].x;
+		sumY += polygon[i].y;
 	}
-	double min_d;
-	min_d = 0;
-	struct Point B;
+	double centerX = sumX / n;
+	double centerY = sumY / n;
+	double rast[n];
 	for(int i = 0; i < n; i++){
-		double rast1 = distance(A, polygon[i]);
-		if(rast1 > min_d){
-			min_d = rast1;
-			B = polygon[i];
-		}
-		
-	}
-	return min_d;
+		rast[i] = sqrt(pow(polygon[i].x - centerX, 2) + pow(polygon[i].y - centerY, 2));
 
+	}
+	double min_d = rast[0];
+	for (int i = 1; i < n; i++) {
+		if (rast[i] < min_d) {
+			min_d = rast[i];
+		}
+	}
+
+	return 2 * min_d;
 }
-//Функция находит самую левую и самую правую точки, для которых ищет расстояниие
+
+//Функция находит самую левую и самую правую точки, для которых ищем расстояниие
 double maxDim(struct Point polygon[], int n){
-double max_d = 0;
+	double max_d = 0;
 	for(int i = 0; i < n; i++){
 		for(int j = i + 1; j < n; j++){
 			struct Point p1 = polygon[i];
@@ -87,13 +63,13 @@ double max_d = 0;
 						if(rast2 > next_rust){
 							next_rust = rast2;
 						}
-					
+
 					}
 				}
 			}
 			if(next_rust > max_d)
 				max_d = next_rust;
-			
+
 		}
 	}
 	return max_d;
@@ -104,12 +80,23 @@ int main(void){
 	double minD;
 	double maxD;
 	printf("Введите количество точек: ");
-	scanf("%d", &n);
+	if(scanf("%d", &n)!=1){
+		printf("Ошибка считывания\n");
+		return 0;
+
+	}
+	if(n<0){
+		printf("Отрицательное значение");
+		return 0;
+	}
 	struct Point polygon[n];
 	printf("Введите координаты точек: \n");
 	for(int i = 0; i < n; i++){
 		printf("Точка %d", i + 1);
-		scanf("%lf %lf", &polygon[i].x, &polygon[i].y);
+		if(scanf("%lf %lf", &polygon[i].x, &polygon[i].y)!=2){
+			printf("Введено неправильне количество координат");
+			return 0;
+		}
 	}
 	minD = minDim(polygon, n);
 	maxD = maxDim(polygon, n);
