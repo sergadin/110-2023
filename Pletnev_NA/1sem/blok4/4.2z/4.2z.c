ccd76f670183145935a1e4477886dc4a821e6f08
Nikolay Pletnev <nikolai.pletnev@math.msu.ru>
2023-12-23 22:03:47 +0300

    4634643

--- Pletnev_NA/1sem/blok4/4.2z/4.2z.c
+++ Pletnev_NA/1sem/blok4/4.2z/4.2z.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#define SIZE (sizeof(unsigned int)*8) 
 
 void Mirror_Coup(int x);
+int Bit_Len(unsigned int y);
+
+
+int Bit_Len(unsigned int y)
+{
+	int d = 0;
+	bool i;
+
+	for (int k = SIZE - 1; k >= 0; k--)
+	{
+		i = ((1 << k) & y);
+		if (i == 1)
+		{
+			d = k + 1;
+			break;
+		}
+	}
+
+	return d;
+}
+
+
+
 
 void Mirror_Coup(int x)
 {
-	int n = ceil(log2(x+1));
+	int n = Bit_Len(x);
 
-	printf("Mirror inverted broken representation of the number (%d):\n", x);
+	printf("Direct binary notation of a number:\n", x);
 
-	for (int k = 0; k < n; k++)
+	for (int k = n - 1; k >= 0; k--)
 	{
 		if ((1 << k) & x)
 		{
@@ -20,6 +45,33 @@ void Mirror_Coup(int x)
 		{
 			printf("%d", 0);
 		}
+
+		if ((k % 4) == 0)
+		{
+			printf("_");
+		}
+	}
+	printf("\n");
+
+
+	printf("Reverse binary notation of a number:\n", x);
+
+	for (int k = 0; k < n; k++)
+	{
+		if ((k % 4) == 0)
+		{
+			printf("_");
+		}
+
+		if ((1 << k) & x)
+		{
+			printf("%d", 1);
+		}
+		else
+		{
+			printf("%d", 0);
+		}
+
 	}
 	printf("\n");
 
