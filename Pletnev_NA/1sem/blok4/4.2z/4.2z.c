1159de09fa60bc81898a6d3c7749029d9ffa3720
Nikolay Pletnev <nikolai.pletnev@math.msu.ru>
2023-12-28 17:05:19 +0300

    rtertr

--- Pletnev_NA/1sem/blok4/4.2z/4.2z.c
+++ Pletnev_NA/1sem/blok4/4.2z/4.2z.c
@@ -2,91 +2,79 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define SIZE (sizeof(unsigned int)*8) 
+#define SIZE (sizeof(unsigned int) * 8)
 
-void Mirror_Coup(int x);
+int Mirror_Coup(unsigned int x);
 int Bit_Len(unsigned int y);
+void Print_Bit(unsigned int x, int n);
 
+void Print_Bit(unsigned int x, int n)
+{
+	for (int k = n - 1; k >= 0; k--)
+	{
+		if ((k % 8) == 0)
+		{
+			printf("_");
+		}
+
+		if ((1 << k) & x)
+		{
+			printf("%d", 1);
+		}
+		else
+		{
+			printf("%d", 0);
+		}
+	}
+
+	printf("\n");
+}
 
 int Bit_Len(unsigned int y)
 {
-	int d = 0;
-	bool i;
 
 	for (int k = SIZE - 1; k >= 0; k--)
 	{
-		i = ((1 << k) & y);
-		if (i == 1)
+		if (((1 << k) & y) != 0)
 		{
-			d = k + 1;
-			break;
+			return k + 1;
 		}
 	}
-
-	return d;
 }
 
-
-
-
-void Mirror_Coup(int x)
+int Mirror_Coup(unsigned int x)
 {
 	int n = Bit_Len(x);
+	bool e1;
+	bool e2;
 
-	printf("Direct binary notation of a number:\n", x);
-
-	for (int k = n - 1; k >= 0; k--)
+	for (int k = n - 1; k >= n / 2; k--)
 	{
-		if ((1 << k) & x)
-		{
-			printf("%d", 1);
-		}
-		else
-		{
-			printf("%d", 0);
-		}
+		e1 = ((1 << k) & x);
+		e2 = ((1 << (n - 1 - k)) & x);
 
-		if ((k % 4) == 0)
-		{
-			printf("_");
-		}
+		x = (e2 << k) | (~(1 << k)) & x;
+		x = (e1 << (n - 1 - k)) | (~(1 << n - 1 - k)) & x;
 	}
-	printf("\n");
 
-
-	printf("Reverse binary notation of a number:\n", x);
-
-	for (int k = 0; k < n; k++)
-	{
-		if ((k % 4) == 0)
-		{
-			printf("_");
-		}
-
-		if ((1 << k) & x)
-		{
-			printf("%d", 1);
-		}
-		else
-		{
-			printf("%d", 0);
-		}
-
-	}
-	printf("\n");
-
-	return;
+	return x;
 }
-
 
 int main(void)
 {
-	int x;
+	unsigned int x;
+	int n;
 
 	printf("Enter the number:\n");
 	scanf("%d", &x);
 
-	Mirror_Coup(x);
+	n = Bit_Len(x);
+
+	printf("Direct binary notation of a number (%d):\n", x);
+	Print_Bit(x, n);
+
+	printf("Reverse binary notation of a number: (%d):\n", x);
+	Print_Bit(Mirror_Coup(x), n);
 
 	return 0;
 }
