cee1411ceea137eeb6836b88e0dac1045e3a462b
maria-popel <143882098+maria-popel@users.noreply.github.com>
2024-02-24 21:03:51 +0300

    исправлена задача 5

---
+++ Popel_MS/2sem/Pointers/z_5/main.c
@@ -0,0 +1,51 @@
+#include <math.h>
+#include "z5.h"
+#include <stdio.h>
+
+typedef struct{  /** новая переменаая-структура для проведения теста */
+	RRFun f; //функция №1
+	RRFun g; //функция №2
+	double x; //действительное значение
+	double res; //ожидаемый результат
+	Error err; //адрес ошибки
+}TestCase;
+
+static double constant(double x); /** Вспомогательная функция для тестирования: константа*/
+static double near_to_zero(double x); /** Вспомогательная функция для тестирования: прибавляет маленькое значение (используется вблизи нуля)*/
+static double sum_1(double x);  /** Вспомогательная функция для тестирования: прибавляет к действительному числу 1*/
+static double substract_2(double x); /** Вспомогательная функция для тестирования: вычитает из действительного числа 2*/
+
+static double substract_2(double x){
+	return x - 2;
+}
+
+static double constant(double x){  
+	return x;
+}
+
+static double near_to_zero(double x){  
+	return x+=1e-5;
+}
+static double sum_1(double x){ 
+	return x+1;
+}
+
+int main(void){
+	double res, eps = 1e-7;
+	Error err;
+	TestCase tests[] = {{sin, cos, 0., 1, NA_OK}, {sin, substract_2, 0.02, 0., NA_LIMIT}, {constant, sin, -0.5, 0., NA_LIMIT}, {sum_1, near_to_zero, -10000-(1e-6),-(1e-6), NA_OK}};
+	int n_tasks = sizeof(tests) / sizeof(tests[0]);
+	for (int n = 0; n < n_tasks; n++){
+		res = composing_with_requisition(tests[n].f, tests[n].g, tests[n].x, eps, &err);
+		if(err != tests[n].err){
+			printf("Тест №%d не пройден.\n", n+1);
+		}else if((err == NA_OK) && (res-tests[n].res) > eps){
+			printf("Тест №%d не пройден.\n", n+1);
+		}else if((err == NA_OK) && (res-tests[n].res) < eps){
+			printf("Тест №%d успешно пройден. Первое подходящее значение у = %lf\n", n+1, res);
+		}else{
+			printf("Тест №%d успешно пройден.\n", n+1);
+		}
+	}
+	return 0;	
+}
---
+++ Popel_MS/2sem/Pointers/z_5/z5.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "z5.h"
+
+/**	Параметры: f - первая действительнозначная функция (на вход подаются ТОЛЬКО функции из R в R!)
+ **		   g - вторая действительнозначная функция
+ **		   х - аргумент
+ **		   eps - погрешность
+ **		   err - код ошибки, по которому будет проводиться проверка
+ **	Функция получает значения из функции тестирования composing_with_requisition, проверяет правильность выполнения теста. */
+	
+double composing_with_requisition(RRFun f, RRFun g, double x, double eps, Error *err){
+	double y, g_y, y_inter; /** соответственно: значение у, значение g(y), промежуточное значение у*/
+	int limit = 10000; /** ограничение по количеству композиций функции f*/
+	y = (*f)(x); 
+	g_y = (*g)(y);
+	
+	while ((g_y < 0.) && (limit > 0)){
+		y_inter = (*f)(y);
+		g_y = (*g)(y_inter);
+		y = y_inter;
+		limit -= 1;
+	}
+	if(limit == 0){
+		*err = NA_LIMIT;
+	}else{
+		*err = NA_OK;
+	}
+	return y;
+}
+ 
