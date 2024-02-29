1bd0b08f9eb09ed16422a8e525b9a8244742d910
solaire758 <144099475+solaire758@users.noreply.github.com>
2024-02-29 19:54:48 +0300

    добавлен комментарий к функции в реализации.

--- Antonov_DP/2sem/functions/z1/arrfun_val.c
+++ Antonov_DP/2sem/functions/z1/arrfun_val.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "arrfun_val.h"
 
+//* Действие функции объясняется в заголовочном файле.
+
 void arrfun_val (RRFun f, double *arr, int len)
 {
 	for (int i = 0; i < len; i++)
