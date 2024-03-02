bd4d4303fcec245e1049bd48c41684e1134b29db
maria-popel <143882098+maria-popel@users.noreply.github.com>
2024-03-02 17:47:45 +0300

    Добавлена задача 1.3(простейшие вычислительные задачи)

---
+++ Popel_MS/2sem/Computational_tasks/z1_3/z1_3.c
@@ -0,0 +1,71 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "z1_3.h"
+
+/*	Параметры: f - действительнозначная функция
+ *		   a - действительное число, левая граница 
+ *		   b - действительное число, правая граница
+ *		   eps - погрешность
+ *		   *err - код ошибки, по которому будет проводиться проверка
+ *	Функция получает значения структуры для тестирования, проверяет правильность выполнения теста. 
+ */
+	
+double chord_method(RRFun f, double a, double b, double eps, Error *err){
+	double x_i,f_xi, f_a = (*f)(a), f_b = (*f)(b);
+	int limit = 10000000;
+    if(a>b){
+        *err = NA_WRNG_ORD;
+        return -1;
+    }
+    if(fabs(f_b-f_a)<eps){
+        *err = NA_EQUAL_VAL;
+        return -1;
+    }
+    if (f_a*f_b>0){
+        *err = NA_NO_ROOT;
+        return -1;
+        }
+    x_i = a - ((f_a*(b-a))/(f_b-f_a));
+
+    f_xi = (*f)(x_i);
+    if (fabs(f_b)<eps){
+        *err = NA_OK;
+        return b;
+    }else if (fabs(f_a)<eps){
+        *err = NA_OK;
+        return a;
+    }else if(fabs(f_xi)<eps){
+        *err = NA_OK;
+        return x_i;
+    }
+    while((fabs(f_xi)>eps) && (limit>0)){
+        if ((f_xi*f_b)<=0){
+
+            a = x_i;
+            f_a = f_xi;
+        }else if ((f_xi*f_a)<=0){
+            b = x_i;
+            f_b = f_xi;
+        }else{
+            *err = NA_NO_ROOT;
+            return -1;
+        }
+
+        if(fabs(f_b-f_a)<eps){
+            *err = NA_EQUAL_VAL;
+            return -1;
+        }
+        x_i = a - ((f_a*(b-a))/(f_b-f_a));
+        f_xi = (*f)(x_i);
+        limit-=1;
+
+    }
+    if (limit==0){
+    	*err = NA_NO_ROOT;
+    	return -1;
+    }
+    *err = NA_OK;
+    return x_i;
+
+}
