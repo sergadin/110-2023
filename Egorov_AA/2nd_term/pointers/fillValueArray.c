35dc5c377c2e395c35f9db6dc202ee1496dc1569
Егоров Александр <pynyashkx@gmail.com>
2024-03-29 00:25:52 +0300

    добавил мейк файл

--- Egorov_AA/2nd_term/pointers/fillValueArray.c
+++ Egorov_AA/2nd_term/pointers/fillValueArray.c
@@ -5,18 +5,20 @@ double* fillValueArray(RRFun* functions, int func_num, double x, error* err) {
 	double* value_array;
 	value_array = (double*)malloc(func_num*sizeof(double));
 	if (value_array == NULL) {
+		printf("Память не выделена\n");
 		*err = MALLOC_ERR;
-		return -1;
+		return 0;
 	}
 	for (int i = 0; i < func_num; i++) {
 		double res;
 		res = functions[i](x);
-		if (isnan(res) || isinf(res)) {
+		if (isnan(res) || isinf(res)) {                // функции из библиотеки math.h. Если значение неопределено(NaN) 
+			printf("Неверный аргумент\n");         // или стремится к бесконечности(inf), функция выдаст ошибку.
 			*err = INCORRECT_ARGUMENT;
-			return -1;
+			free(value_array);
+			return 0;
 		}
 		value_array[i] = res;
-		printf("%lf\n", value_array[i]);
 	}
 	*err = OK;
 	return value_array;
