67af75df7652ec85f324e655ec77ea595d703902
maria-popel <143882098+maria-popel@users.noreply.github.com>
2024-03-28 18:28:33 +0300

    Update z2_3.c

--- Popel_MS/2sem/Computational_tasks/z2_3/z2_3.c
+++ Popel_MS/2sem/Computational_tasks/z2_3/z2_3.c
@@ -11,6 +11,21 @@
  *      Функция подсчитывает приближенное значение интеграла функции f(x) с помощью метода Симпсона с заданным разбиением.
  */
 static double Integrate(RRFun f, double a, double b, double n);
+static double max(double x, double y, double eps);
+
+static double max(double x, double y, double eps){
+	double max_1;
+	if (x>y){
+	max_1 = x;
+	}else{
+		max_1 = y;
+	}
+	if (eps>max_1){
+		return eps;
+	}
+	return max_1;
+}
+
 static double Integrate(RRFun f, double a, double b, double n){
 	double dx, summa; /* Соответственно: подотрезок разбиения, интегральная сумма.*/
 	int k = 4;
@@ -39,7 +54,7 @@ double Simpson_method(RRFun f, double a, double b, double eps, Error *err){
     }
     s1 = Integrate(f, a, b, n);
     n = 2048;
-    while (fabs(s1 - s2)>eps){
+    while (fabs(s1 - s2)>(max(s1, s2, 1.0) * eps)){
         s2 = Integrate(f, a, b, n);
         s1 = s2;
         n = 2*n;
