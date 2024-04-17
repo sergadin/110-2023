7fc5088a7f6f9a9b3faac59ce211b20e4adaa996
pynyashko <pynyashkx@gmail.com>
2024-04-18 00:26:17 +0300

    исправил тест программы

---
+++ Egorov_AA/2nd_term/pointers/main.c
@@ -0,0 +1,96 @@
+﻿#include "fillValueArray.h" 
+
+// Заданные функции
+double sine(double x);
+double cubing(double x);
+double reverseNum(double x);
+double naturalLog(double x);
+
+// Структура тестов
+typedef struct {
+	RRFun* funcs;     // Указатель на массив указателей на функции :)
+	double x;         // Аргумент
+	double* res;      // Указатель на массив ожидаемых значений
+	error err_code;   // Ожидаемый код ошибки
+} dataSet;
+
+double sine(double x) {
+	return sin(x);
+}
+
+double cubing(double x) {
+	return x * x * x;
+}
+
+double reverseNum(double x) {
+	return 1 / x;
+}
+
+double naturalLog(double x) {
+	return log(x);
+}
+
+int main(void) {
+	const double pi = 3.1415926535, eps = 1e-4;
+	int test_num, func_num = 4;
+	error err;
+	double* value;
+
+	dataSet tests[] = {                                                       // Tесты
+		{
+			(RRFun[]) {sine, cubing, reverseNum, naturalLog},         // Функции
+			pi / 2,                                                   // Аргумент
+			(double[]) {1.0, 3.87578, 0.63662, 0.451583},             // Ожидаемые значения
+			MALLOC_ERR                                                // Ожидаемый код ошибки
+		},
+		{
+			(RRFun[]) {sine, cubing, reverseNum, naturalLog},
+			pi / 2,
+			(double[]) {1.0, 3.87578, 0.63662, 0.451583},
+			OK
+		},
+		{
+			(RRFun[]) {sine, cubing, reverseNum, naturalLog},
+			0,
+			(double[]) {1.0, 3.87578, 0.63662, 0.451583},
+			INCORRECT_ARGUMENT
+		},
+		{
+			(RRFun[]) {sine, cubing, reverseNum, naturalLog},
+			-1,
+			(double[]) {1.0, 3.87578, 0.63662, 0.451583},
+			INCORRECT_ARGUMENT
+		}
+	};
+
+	test_num = sizeof(tests) / sizeof(tests[0]);
+
+	fillValueArray(tests[0].funcs, -1, tests[0].x, &err);                    // Отдельно выделен первый тест.
+	if (err == MALLOC_ERR)                                                   // На вход подается неправильное количество функций
+		printf("1-й тест пройден :)\n");                                 // для умышленной ошибки выделения памяти.
+	else
+		printf("1-й тест не пройден:(\n");
+
+	for (int i = 1; i < test_num; i++) {
+		value = fillValueArray(tests[i].funcs, func_num, tests[i].x, &err);
+		if (err != tests[i].err_code) {
+			printf("%d-й тест не пройден :(\n", i + 1);
+		}
+		else if (err == OK) {
+			for (int j = 0; j < func_num; j++) {
+				if (fabs(value[j] - tests[i].res[j]) / 2 > eps) {
+					printf("%d-й тест не пройден :(\n", i + 1);
+				}
+			}
+		}
+		else {
+			printf("%d-й тест пройден :)", i + 1);
+		}
+
+		if(err != MALLOC_ERR)
+			free(value);
+	}
+	
+	
+	return 0;
+}
