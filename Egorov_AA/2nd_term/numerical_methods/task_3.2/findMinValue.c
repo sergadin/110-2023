8e4a9f398bebd91b63903aa39aa61e1699b2a870
pynyashko <pynyashkx@gmail.com>
2024-04-19 21:20:20 +0300

    Исправило программу

--- Egorov_AA/2nd_term/numerical_methods/task_3.2/findMinValue.c
+++ Egorov_AA/2nd_term/numerical_methods/task_3.2/findMinValue.c
@@ -1,9 +1,17 @@
 #include "findMinValue.h"
 
 double findMinValue(RRFun f, double a, double b, double eps, error* err) {
-	const double phi = 0.38196;
-	while ((fabs((*f)(b) - (*f)(a)) > eps * fabs((*f)(a) + (*f)(b)) / 2) || (fabs((*f)(b) - (*f)(a)) > eps)) {
-		double x1, x2;
+	const double phi = 0.38196;      // Отношение, в котором отрезок разбивается
+	int limit = 10000;               // Ограничение на количество итераций
+
+	if (a > b) {
+		*err = WRONG_INTERVAL;       // Проверка ошибки неверного задания интервала 
+		return -1;
+	}
+
+	while ((fabs((*f)(b) - (*f)(a)) > eps * fabs((*f)(a) + (*f)(b)) / 2) ||    // Реализованный метод
+		(fabs((*f)(b) - (*f)(a)) > eps)) {
+		double x1, x2;                                                       
 
 		x1 = a + phi * (b - a);
 		x2 = b - phi * (b - a);
@@ -14,9 +22,15 @@ double findMinValue(RRFun f, double a, double b, double eps, error* err) {
 		else {
 			a = x1;
 		}
-
+		limit--;
 	}
-	printf("Minimum: %lf\n", fabs((*f)(a) - (*f)(b)));
+
+	if (limit <= 0) {               // Проверка ошибки на превышение количества итераций
+		*err = I_LIMIT_EXCEEDED;
+		return -1;
+	}
+
+	printf("Minimum: %lf\n", fabs((*f)((b + a) / 2)));
 	*err = OK;
-	return ((*f)(a) + (*f)(b)) / 2;
+	return (*f)((b + a) / 2);
 }
--- Egorov_AA/2nd_term/numerical_methods/task_3.2/main.c
+++ Egorov_AA/2nd_term/numerical_methods/task_3.2/main.c
@@ -2,17 +2,18 @@
 #include <math.h>
 #include "findMinValue.h"
 
-typedef struct {
-	RRFun func;
-	double a;
-	double b;
-	double res;
-	error err_code;
+typedef struct {          // Структура тестов:
+	RRFun func;           //   - Указатель на функцию
+	double a;             //   - Начало отрезка
+	double b;             //   - Конец отрезка
+	double res;           //   - Ожидаемый результат
+	error err_code;       //   - Ожидаемая ошибка
 } dataSet;
 
-double func1(double x);
+double func1(double x);      // Заданные функции
 double func2(double x);
 double func3(double x);
+double func4(double x);
 
 
 double func1(double x) {
@@ -30,22 +31,27 @@ double func3(double x) {
 }
 
 
+double func4(double x) {
+	return 5;
+}
+
+
 int main(void) {
 	int test_num;
 	error err;
 	double value;
 	const double eps = 1e-3;
-	dataSet tests[] = {
-		{func1, -0.5, 5, 3.938, OK},
-		{func2, -10, 10, -52, OK},
-		{func3, -10, 10, 7, OK},
-		{func3, -0.5, 5, 0.803, OK},
-		{func3, -0.5, 5, 0.803, OK}
+	dataSet tests[] = {                                   //      Тесты
+		{func1, -0.5, 5, 3.938, OK},                      // { Имя функции, 
+		{func2, -10, 5, -52, OK},                         //   Начало отрезка,
+		{func3, -10, 10, 7, OK},                          //   Конец отрезка,
+		{func3, 5, 0, 0.803, WRONG_INTERVAL},             //   Ожидаемое значение,
+		{func4, -1000, 1000, 5, OK}                       //   Ожидаемая ошибка }
 	};
 
 	test_num = sizeof(tests) / sizeof(tests[0]);
 
-	for (int i = 0; i < test_num; i++) {
+	for (int i = 0; i < test_num; i++) {                                            // Само тестирование
 		value = findMinValue(tests[i].func, tests[i].a, tests[i].b, eps, &err);
 		if (err != tests[i].err_code) {
 			printf("%d-й тест не пройден :(\n", i + 1);
@@ -57,7 +63,6 @@ int main(void) {
 			printf("%d-й тест пройден :)\n", i + 1);
 		}
 	}
-
 
 	return 0;
 }
