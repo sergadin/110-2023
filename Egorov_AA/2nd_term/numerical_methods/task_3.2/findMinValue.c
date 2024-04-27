6d213bab273846319a0a7e77fa14d99fc2ca660d
pynyashko <pynyashkx@gmail.com>
2024-04-27 21:51:33 +0300

    изменил цикл while

--- Egorov_AA/2nd_term/numerical_methods/task_3.2/findMinValue.c
+++ Egorov_AA/2nd_term/numerical_methods/task_3.2/findMinValue.c
@@ -2,35 +2,44 @@
 
 double findMinValue(RRFun f, double a, double b, double eps, error* err) {
 	const double phi = 0.38196;      // Отношение, в котором отрезок разбивается
-	int limit = 10000;               // Ограничение на количество итераций
+	int limit = 1000;               // Ограничение на количество итераций
+	double f_x1, f_x2, x1, x2;
 
 	if (a > b) {
 		*err = WRONG_INTERVAL;       // Проверка ошибки неверного задания интервала 
 		return -1;
 	}
 
-	while ((fabs((*f)(b) - (*f)(a)) > eps * fabs((*f)(a) + (*f)(b)) / 2) ||    // Реализованный метод
-		(fabs((*f)(b) - (*f)(a)) > eps)) {
-		double x1, x2;                                                       
+	x1 = a + phi * (b - a);
+	x2 = b - phi * (b - a);
+	f_x1 = (*f)(x1);
+	f_x2 = (*f)(x2);
 
-		x1 = a + phi * (b - a);
-		x2 = b - phi * (b - a);
+	while ((fabs(f_x1 - f_x2) > eps) || (fabs(f_x1 - f_x2) > eps * fabs(f_x1 + f_x2) / 2)) {
 
-		if ((*f)(x1) < (*f)(x2)) {
+		if (f_x1 < f_x2) {
 			b = x2;
+			x2 = x1;
+			x1 = a + phi * (b - a);
+			f_x2 = f_x1;
+			f_x1 = (*f)(x1);
 		}
 		else {
 			a = x1;
+			x1 = x2;
+			x2 = b - phi * (b - a);
+			f_x1 = f_x2;
+			f_x2 = (*f)(x2);
 		}
+
 		limit--;
+		if (limit <= 0) {               // Проверка ошибки на превышение количества итераций
+			*err = I_LIMIT_EXCEEDED;
+			return -1;
+		}
 	}
 
-	if (limit <= 0) {               // Проверка ошибки на превышение количества итераций
-		*err = I_LIMIT_EXCEEDED;
-		return -1;
-	}
-
-	printf("Minimum: %lf\n", fabs((*f)((b + a) / 2)));
+	printf("Minimum: %lf\n", f_x1);
 	*err = OK;
-	return (*f)((b + a) / 2);
+	return f_x1;
 }
