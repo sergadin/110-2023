7b457f077a8e41da520510600814d762c48b32a5
pynyashko <pynyashkx@gmail.com>
2024-04-28 13:01:34 +0300

    добавил программу

--- Egorov_AA/2nd_term/numerical_methods/task_1.3/findRoot.c
+++ Egorov_AA/2nd_term/numerical_methods/task_1.3/findRoot.c
@@ -1,2 +1,63 @@
-#include "findRoot.h"
+﻿#include "findRoot.h"
 
+double findRoot(RRFun f, double a, double b, double eps, int* iteration_num, error* err) {
+
+	double f_a, f_b, f_c, c[2];
+	int i = 0, limit = 10000;
+
+	if (a > b) {
+		*err = WRONG_INTERVAL;
+		return -1;
+	}
+
+	*err = OK;
+
+	f_a = (*f)(a);
+	f_b = (*f)(b);
+
+	c[0] = a;
+	c[1] = b;
+
+	if (f_a * f_b > 0) {
+		*err = WRONG_FUNCTION;
+		return -1;
+	}
+
+	if (fabs(f_a) < eps) {
+		printf("Корень - %lf\n", c[i]);
+		return a;
+	}
+
+	if (fabs(f_b) < eps) {
+		printf("Корень - %lf\n", c[i]);
+		return b;
+	}
+
+	for (*iteration_num = 1; fabs(c[1] - c[0]) > eps; (*iteration_num)++) {
+		i = !i;
+		c[i] = (a * f_b - b * f_a) / (f_b - f_a);
+		f_c = (*f)(c[i]);
+
+		if (fabs(f_c) < eps) {
+			printf("Корень - %lf\n", c[i]);
+			return c[i];
+		}
+		if (f_c * f_a > 0) {
+			a = c[i];
+			f_a = f_c;
+		}
+		else {
+			b = c[i];
+			f_b = f_c;
+		}
+		
+		limit--;
+		if (limit < 0) {
+			*err = I_LIMIT_EXCEEDED;
+			return -1;
+		}
+	}
+
+	printf("Корень - %lf\n", c[i]);
+	return c[i];
+}
--- Egorov_AA/2nd_term/numerical_methods/task_1.3/main.c
+++ Egorov_AA/2nd_term/numerical_methods/task_1.3/main.c
@@ -27,7 +27,7 @@ double func2(double x) {
 
 
 double func3(double x) {
-	return x * x - 10 * x + 32;
+	return x * x - 10 * x + 20;
 }
 
 
@@ -35,8 +35,36 @@ double func4(double x) {
 	return 5;
 }
 
-
 int main(void) {
-	printf("Привет, мир");
+	system("chcp 1251");
+	int test_num, iteration_num;
+	error err;
+	double value;
+	const double eps = 1e-5;
+	dataSet tests[] = {                                   //      Тесты
+		{func1, -3, 1, -2.110371, OK},                    // { Имя функции, 
+		{func2, -10, 5, 0.4, OK},                         //   Начало отрезка,
+		{func3, 4, 10, 7.236067, OK},                     //   Конец отрезка,
+		{func4, -1000, 1000, 5, WRONG_FUNCTION},          //   Ожидаемое значение,
+		{func3, 10, 20, 0.803, WRONG_FUNCTION},           //   Ожидаемая ошибка }
+		{func3, 5, 0, 0.803, WRONG_INTERVAL}
+	};
+
+	test_num = sizeof(tests) / sizeof(tests[0]);
+
+	for (int i = 0; i < test_num; i++) {                                            // Тестирование
+		iteration_num = 0;
+		value = findRoot(tests[i].func, tests[i].a, tests[i].b, eps, &iteration_num, &err);
+		if (err != tests[i].err_code) {
+			printf("%d-й тест не пройден :(  %d\n", i + 1, iteration_num);
+		}
+		else if ((err == OK) && (fabs(tests[i].res - value) / 2 > eps)) {
+			printf("%d-й тест не пройден :(  %d\n", i + 1, iteration_num);
+		}
+		else {
+			printf("%d-й тест пройден. Количество итераций - %d\n", i + 1, iteration_num);
+		}
+	}
+
 	return 0;
 }
