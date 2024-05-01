f9d1b9976a63f9d9117aa071e6d2f0dbbbbe1d30
alinagelman <lina_art_raccon@mail.ru>
2024-05-01 12:32:25 +0300

    Task1
    llalalall

---
+++ Gelman_AD/Semester2/Task1/task_1.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Header.h"
+
+/*	Parameters:
+	f - real functiom (to the enter comes ONLY functions from R to R)
+	x - argument
+	Function main_function gets the x-es from the Main.c and the test function (example: square) from the Main.c 
+*/
+
+void main_function(RRFun f, double* x, int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		x[i] = f(x[i]);
+	}
+}
