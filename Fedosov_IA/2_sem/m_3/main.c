b300e4f739b81718848b8a104a8aa6441ef45259
Федосов Игорь Александрович <igor.fedosov@math.msu.ru>
2024-05-04 09:10:53 +0300

    добавил решение 3 задачи на матрицы

---
+++ Fedosov_IA/2_sem/m_3/main.c
@@ -0,0 +1,41 @@
+#include "get_inverse.h"
+#include <stdio.h>
+
+
+typedef struct{  
+    const char * file;
+    int size;
+} TestCase;
+
+
+int main(void) {    
+    Error error;
+    TestCase tests[] = {
+        {"data1.txt", 4},
+        {"data2.txt", 2},
+        {"data3.txt", 3},
+        {"data4.txt", 3},
+        {"data5.txt", 4},
+        {"data6.txt", 3}
+    };
+
+    double* rez;
+
+    for(int i = 0; i < 6; i++) {
+        rez = inverse(tests[i].file, &error);
+        if(error == NO_INVERSE) {
+            printf("В тесте %d обратной матрицы не существует тк определитель исходной равен 0\n", i + 1);
+        } else if(error == EMPTY_FILE) {
+            printf("В тесте %d файл с входными данными пуст\n", i + 1);
+        } else if (error == OK) {
+            for (int k = 0; k < tests[i].size; k++) {
+                for (int j = 0; j < tests[i].size; j++) {
+                    printf("%lf ", rez[k * tests[i].size + j]);
+                }
+                printf("\n");
+            }
+        }
+        printf("\n");
+    }
+    return 1;
+}
