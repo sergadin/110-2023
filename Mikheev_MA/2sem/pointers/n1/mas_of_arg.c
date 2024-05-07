97121c82dd7366a09273e2eaf27a1c17933a8212
maxmiheev <makcmiheeb141@gmail.com>
2024-05-07 20:28:02 +0300

    коррекция
    коррекция

--- Mikheev_MA/2sem/pointers/n1/mas_of_arg.c
+++ Mikheev_MA/2sem/pointers/n1/mas_of_arg.c
@@ -1,9 +1,12 @@
 #include "mas_of_arg.h"
 
-void mas_of_arg(RRFun f, double x, double *mas_arg, Er *error, int i){
-    mas_arg[i] = f(x);
-    if(isnan(mas_arg[i]) || isinf(mas_arg[i])){       // если значение стремится в бесконечность или неопределено
+void mas_of_arg(RRFun f, double *mas_arg, Er *error, int length){
+    
+    for(int i = 0; i < length; i++){
+        mas_arg[i] = f(mas_arg[i]);
+        if(isnan(mas_arg[i]) || isinf(mas_arg[i])){       // если значение стремится в бесконечность или неопределено
         printf("Некорректный аргумент для данной функции в тесте №%d\n", i + 1);
         *error = NOT_OK;
+        }
     }
 }
