8627545f6bd55a6b210055d60e7c8aa99efad089
Антонов Дмитрий <dim.pav.ant@gmail.com>
2024-05-08 20:08:36 +0300

    удалён ненужный файл, начато решение третьей задачи

--- Antonov_DP/2sem/min_value/min_value.c
+++ Antonov_DP/2sem/min_value/min_value.c
@@ -2,36 +2,30 @@
 #include <math.h>
 #include "integration.h"
 
+static int compRR(double a, double b, double eps);
 
-double integrate(RRFun f, double a, double b, double eps, ERR *err)
+static int compRR(double a, double b, double eps)
 {
-	int count = 8; //количество отрезков, на которые разбивается отрезок
-	double length, f_start, f_end, start, end, prev_integr = f(a)*(b - a), curr_integr;
-	if (a > b) 
-	{ 
-		*err = E_IO;
-		printf("ошибка, некорректно введён отрезок \n");
-		return 0;
-	}
-	do
-	{
-		length = (b - a) / count;
-		start = a;
-		end = a + length;
-		f_start = f(start);
-		f_end = f(end);
-		prev_integr = curr_integr;
-		curr_integr = 0;
-		for (int i = 0; i < count; i++)
-		{
-			curr_integr += ((f_start + f_end) * length) / 2;
-			start = end;
-			end += length;
-			f_start = f_end;
-			f_end = f(end);
-		}
-		count = count * 2;
-	}
-	while ((fabs(prev_integr - curr_integr) > eps) || (length > eps));
-	return curr_integr;
+        if((a > 100000) || (b > 100000))
+        {
+                if (fabs(a - b) < eps * (a + b)) { return 0; }
+                if (a > b) { return 1; }
+                return -1;
+        }
+        if (fabs(a - b) < eps) { return 0; }
+        if (a > b) { return 1; }
+        return -1;
+}
+
+static double min_value_parab(double x1, double y1, double x2, double y2, double x3, double y3, double eps);
+
+static double min_value_parab(double x1, double y1, double x2, double y2, double x3, double y3, double eps)
+{
+	if (compRR(y1, y3, eps) != 0) { return y2; }
+
+}
+
+
+double min_value(RRFun f, double a, double b, double eps, ERR *err)
+{
 }
--- Antonov_DP/2sem/min_value/min_value.h
+++ Antonov_DP/2sem/min_value/min_value.h
@@ -5,9 +5,9 @@ typedef double (*RRFun)(double x);
 
 typedef enum { OK = 0, E_IO } ERR;
 
-// функция находит интеграл f на отрезке [a;b] методом трапеций
+// функция находит минимум функции f на отрезке [a;b] методом параболы, с помощью разбиения равномерной сетки
 // a - начало отрека и b - конец отрека a < b
-// eps - точность интеграла
+// eps - точность минимума
 // все данные типа double
 
 double min_value (RRFun f, double a, double b, double eps, ERR *err);
