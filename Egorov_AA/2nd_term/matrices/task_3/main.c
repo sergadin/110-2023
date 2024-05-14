a4b71382ebd8cf5689afedcaa139965c21f130a6
pynyashko <pynyashkx@gmail.com>
2024-05-14 22:56:51 +0300

    Упростил функции до случая квадратных матриц,, добавил функцию перемножения матриц

--- Egorov_AA/2nd_term/matrices/task_3/main.c
+++ Egorov_AA/2nd_term/matrices/task_3/main.c
@@ -4,26 +4,32 @@
 
 
 int main() {
-	int m, n;
-	double** matrix, ** inverse_matrix;
+	int n;
+	double** matrix, ** inverse_matrix, ** multiplied_matrix;
+	system("chcp 1251");
 	printf("Введите размер квадратной матрицы NxN:");
 	scanf("%d", &n);
-	m = n;
 	matrix = (double**)malloc(n * sizeof(double*));
 	for (int i = 0; i < n; i++) {
 		matrix[i] = (double*)malloc(n * sizeof(double));
 	}
-	fillMatrix(&matrix, n, n);
+	fillMatrix(&matrix, n);
 
 	printf("\nСгенерированная матрица размера %dx%d:\n", n, n);
-	writeMatrix(matrix, n, n);
+	writeMatrix(matrix, n);
 
 	inverse_matrix = invertMatrix(matrix, n);
 
 	printf("\nОбратная матрица:\n");
 	writeMatrix(inverse_matrix, n, n);
-	
-	for (int i = 0; i < m; i++) {
+
+	multiplied_matrix = multiplyMatrices(matrix, inverse_matrix, n);
+
+	printf("\nРезультат перемножения:\n");
+
+	writeMatrix(multiplied_matrix, n);
+
+	for (int i = 0; i < n; i++) {
 		free(matrix[i]);
 	}
 	free(matrix);
@@ -31,5 +37,9 @@ int main() {
 		free(inverse_matrix[i]);
 	}
 	free(inverse_matrix);
+	for (int i = 0; i < n; i++) {
+		free(multiplied_matrix[i]);
+	}
+	free(multiplied_matrix);
 	return 0;
 }
--- Egorov_AA/2nd_term/matrices/task_3/matrix.c
+++ Egorov_AA/2nd_term/matrices/task_3/matrix.c
@@ -1,15 +1,15 @@
 ﻿#include "matrix.h"
 
-void fillMatrix(double*** matrix, int m, int n) {
+void fillMatrix(double*** matrix, int n) {
 	srand(time(NULL));
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			(*matrix)[i][j] = (double)rand();
 }
 
 
-void writeMatrix(double** matrix, int m, int n) {
-	for (int i = 0; i < m; i++) {
+void writeMatrix(double** matrix, int n) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++)
 			printf("%26.14lf ", matrix[i][j]);
 
@@ -18,33 +18,59 @@ void writeMatrix(double** matrix, int m, int n) {
 }
 
 
-double** invertMatrix(double** matrix, int n) {
-	double** inverse_matrix;
-	inverse_matrix = (double**)malloc(n * sizeof(double*));
+double** multiplyMatrices(double** matrix1, double** matrix2, int n) {
+	double** multiplied_matrix;
+	multiplied_matrix = (double**)malloc(n * sizeof(double*));
+	for (int i = 0; i < n; i++)
+		multiplied_matrix[i] = (double*)malloc(n * sizeof(double));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			multiplied_matrix[i][j] = 0;
+			for (int k = 0; k < n; k++) {
+				multiplied_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
+		}
+	}
+
+	return multiplied_matrix;
+}
+
+
+double** invertMatrix(double** given_matrix, int n) {
+	double** inverse_matrix, ** matrix;
+	inverse_matrix = (double**)malloc(n * sizeof(double*));      // Выделение памяти на матрицы
 	for (int i = 0; i < n; i++)
 		inverse_matrix[i] = (double*)malloc(n * sizeof(double));
+	matrix = (double**)malloc(n * sizeof(double*));
+	for (int i = 0; i < n; i++)
+		matrix[i] = (double*)malloc(n * sizeof(double));
 
-	for (int i = 0; i < n; i++)                               // Инициализация единичной матрицы
+	for (int i = 0; i < n; i++)                                  // Копирование матрицы
+		for (int j = 0; j < n; j++)
+			matrix[i][j] = given_matrix[i][j];
+
+	for (int i = 0; i < n; i++)                                  // Инициализация единичной матрицы
 		for (int j = 0; j < n; j++)
 			if (i == j)
 				inverse_matrix[i][j] = 1;
 			else
 				inverse_matrix[i][j] = 0;
 
-	for (int i = 0; i < n; i++) {                             // Метод Гаусса - Жордано
-	
-		double pivot = matrix[i][i];                      // Выбор главного элемента
+	for (int i = 0; i < n; i++) {                                // Метод Гаусса - Жордано
+
+		double pivot = matrix[i][i];                             // Выбор главного элемента
 		if (pivot == 0.0) {
 			printf("Обратная матрица не существует.\n");
 			return 0;
 		}
-		
-		for (int j = 0; j < n; j++) {                     // Деление строки на значение главного элемента
+
+		for (int j = 0; j < n; j++) {                            // Деление строки на значение главного элемента
 			matrix[i][j] /= pivot;
 			inverse_matrix[i][j] /= pivot;
 		}
-		
-		for (int k = 0; k < n; k++) {                     // Обнуление элементов под и над главными элементами
+
+		for (int k = 0; k < n; k++) {                            // Обнуление элементов под и над главными элементами
 			if (k != i) {
 				double factor = matrix[k][i];
 				for (int j = 0; j < n; j++) {
@@ -54,6 +80,11 @@ double** invertMatrix(double** matrix, int n) {
 			}
 		}
 	}
+
+	for (int i = 0; i < n; i++) {
+		free(matrix[i]);
+	}
+	free(matrix);
 
 	return inverse_matrix;
 }
