2c5c9a1d72a0dc2bf114e1834e8e861d99a055ea
alinagelman <lina_art_raccon@mail.ru>
2024-05-18 18:41:01 +0300

    66
    88888888

--- Gelman_AD/Semester2/Task6/interpolation.c
+++ Gelman_AD/Semester2/Task6/interpolation.c
@@ -18,9 +18,7 @@ double linear_interpolation(double x0, double y0, int n, double x1, double y1, d
 void interpolate(point* p, size_t n, point* i_p, size_t m, double* interp_y, error* err)
 {
     *err = OK;
-    double x1[m]; // Points from the net for interpolation
-    double y11[m]; // The value of the function of the these points
-    
+
     for (int j = 0; j < m; j++)
     {
         double x2, y2;
@@ -38,18 +36,8 @@ void interpolate(point* p, size_t n, point* i_p, size_t m, double* interp_y, err
         double x = i_p[j].x;
 
         // Counting the approximate value of the function using the piecewise linear interpolation
-        for (int i = 0; i < m; i++)
-        {
-            y11[i] = linear_interpolation(x0, y0, n, x1, y1, x); // Using the piecewise linear interpolation
-        }
-
-        // Weighting coefficients
-        double L0 = ((x - x1) * (x - x2)) / ((x0 - x1) * (x0 - x2));
-        double L1 = ((x - x0) * (x - x2)) / ((x1 - x0) * (x1 - x2));
-        double L2 = ((x - x0) * (x - x1)) / ((x2 - x0) * (x2 - x1));
-
-        double result = y0 * L0 + y1 * L1 + y2 * L2;
-
+        double result = linear_interpolation(x0, y0, n, x1, y1, x); // Using the piecewise linear interpolation
+        printf("Result: %lf\n", result);
         interp_y[j] = result;
     }
 }
