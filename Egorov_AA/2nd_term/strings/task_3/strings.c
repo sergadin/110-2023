45c2cb7860479d8ba01865a0a45832ed5fe3e1f5
pynyashko <pynyashkx@gmail.com>
2024-05-22 18:35:46 +0300

    убрал комментарии

--- Egorov_AA/2nd_term/strings/task_3/strings.c
+++ Egorov_AA/2nd_term/strings/task_3/strings.c
@@ -1,42 +1,40 @@
 #include "strings.h"
 
 void copyWords(const char* input_filename, const char* output_filename) {
-    FILE* input_file = fopen(input_filename, "r");
-    FILE* output_file = fopen(output_filename, "w");
+	FILE* input_file = fopen(input_filename, "r");
+	FILE* output_file = fopen(output_filename, "w");
 
-    if (input_file == NULL) {
-        perror("Ошибка открытия входного файла");
-        return;
-    }
-    if (output_file == NULL) {
-        perror("Ошибка открытия выходного файла");
-        fclose(input_file);
-        return;
-    }
+	if (input_file == NULL) {
+		perror("Ошибка открытия входного файла");
+		return;
+	}
+	if (output_file == NULL) {
+		perror("Ошибка открытия выходного файла");
+		fclose(input_file);
+		return;
+	}
 
-    int ch;
-    char word[256];  // Буфер для хранения текущего слова
-    int index = 0;
+	int ch;
+	char word[256];
+	int index = 0;
 
-    while ((ch = fgetc(input_file)) != EOF) {
-        if (isspace((unsigned char)ch) || ispunct((unsigned char)ch)) {
-            if (index > 0) {
-                word[index] = '\0';  // Завершаем строку
-                fprintf(output_file, "%s\n", word);  // Записываем слово в выходной файл
-                index = 0;  // Сбрасываем индекс для нового слова
-            }
-        }
-        else {
-            word[index++] = (char)ch;  // Добавляем символ к текущему слову
-        }
-    }
+	while ((ch = fgetc(input_file)) != EOF) {
+		if (isspace((unsigned char)ch) || ispunct((unsigned char)ch)) {
+			if (index > 0) {
+				word[index] = '\0';
+				fprintf(output_file, "%s\n", word);
+				index = 0;
+			}
+		}
+		else {
+			word[index++] = (char)ch;
+		}
+	}
+	if (index > 0) {
+		word[index] = '\0';
+		fprintf(output_file, "%s\n", word);
+	}
 
-    // Проверяем, есть ли слово в буфере после окончания чтения файла
-    if (index > 0) {
-        word[index] = '\0';
-        fprintf(output_file, "%s\n", word);
-    }
-
-    fclose(input_file);
-    fclose(output_file);
+	fclose(input_file);
+	fclose(output_file);
 }
