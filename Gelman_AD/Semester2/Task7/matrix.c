2e2fefd0428c5ac91176c05541734dbee88c8854
alinagelman <lina_art_raccon@mail.ru>
2024-05-22 15:07:38 +0300

    77
    Errors fixed

--- Gelman_AD/Semester2/Task7/matrix.c
+++ Gelman_AD/Semester2/Task7/matrix.c
@@ -9,23 +9,23 @@
 //  n (= order) - dimention of the original matrix (the number of the equasions)
 
 // The function multiplies matrix A by vector x and writes the result to vector result
-void mul_matrix_vector(double A[n][n], double x[n], double result[n]);
-void mul_matrix_vector(double A[n][n], double x[n], double result[n]) 
+void mul_matrix_vector(double** matrix, int n, double* vector_x, double* result_vector);
+void mul_matrix_vector(double** matrix, int n, double* vector_x, double* result_vector)
 {
     for (int i = 0; i < n; i++) 
     {
-        result[i] = 0;
+        result_vector[i] = 0;
 
         for (int j = 0; j < n; j++) 
         {
-            result[i] += A[i][j] * x[j];
+            result_vector[i] += matrix[i][j] * vector_x[j];
         }
     }
 }
 
 // The function scalarly multiplies two vectors x and y
-double dot_product(double x[n], double y[n]);
-double dot_product(double x[n], double y[n]) 
+double dot_product(double* x, double* y, int n);
+double dot_product(double* x, double* y, int n)
 {
     double result = 0;
     for (int i = 0; i < n; i++) 
@@ -35,14 +35,29 @@ double dot_product(double x[n], double y[n])
     return result;
 }
 
-double descent_method(double* matrix, int n, double* vector_b, double* vector_x, double epsilon, Error* error)
+// The function multiplies the transposed matrix A and the column vector x
+void mul_trmatrix_vector(double** matrix, int n, double* vector_x, double* result_vector);
+void mul_trmatrix_vector(double** matrix, int n, double* vector_x, double* result_vector)
 {
-    double A[N][N] = matrix;
-    double b[N] = vector_b;
-    double x[n] = vector_x;
-    double r[n], p[n], Ap[n], alpha, beta;
-    int limit = 1000;
-    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        result_vector[i] = 0;
+        for (int j = 0; j < n; j++) 
+        {
+            result_vector[i] += matrix[j][i] * vector_x[j];
+        }
+    }
+}
+
+double descent_method(double** matrix, int n, double* vector_b, double* vector_x, double epsilon, Error* error)
+{
+    double* result_vector = NULL;
+    double* vector_Ax = NULL;
+    double* tr_vector_A = NULL;
+    double* vector_AAtr = NULL;
+    int limit = 1000; // The limit of the iterations
+    int iteration = 0; // The number of iterations
+    int k = 0; // The coefficient for Gauss method
     *error = OK;
 
     if (n == 0)
@@ -51,47 +66,72 @@ double descent_method(double* matrix, int n, double* vector_b, double* vector_x,
         return -1;
     }
 
-    while (sqrt(rsnew) > epsilon && k < limit)
+    result_vector = (double*)malloc((n) * sizeof(double));
+    vector_Ax = (double*)malloc((n) * sizeof(double));
+    tr_vector_A = (double*)malloc((n) * sizeof(double));
+    vector_AAtr = (double*)malloc((n) * sizeof(double));
+
+    while (sqrt(dot_product(result_vector, result_vector, n)) > epsilon && iteration < limit)
     {
-        mul_matrix_vector(A, x, Ap); // Ap = A * x
+        mul_matrix_vector(matrix, n, vector_x, vector_Ax); // Ap = A * x
+
         for (int i = 0; i < n; i++) 
         {
-            r[i] = b[i] - Ap[i]; // Counting vector r = b - A * x
-            p[i] = r[i]; // Initial search direction
+            result_vector[i] = vector_Ax[i] - vector_b[i];; // Counting vector r = A * x - b
         }
 
-        mul_matrix_vector(A, p, Ap); // Ap = A * p
+        mul_trmatrix_vector(matrix, n, result_vector, tr_vector_A);
+        mul_matrix_vector(matrix, n, tr_vector_A, vector_AAtr);
+
+        if (sqrt(dot_product(vector_AAtr, vector_AAtr, n)) <= epsilon)
+        {
+            break;
+            *error = ZERO_MATRIX; // Divided by zero
+        }
+
+        k = dot_product(result_vector, vector_AAtr, n) / dot_product(vector_AAtr, vector_AAtr, n);
+        for (int i = 0; i < n; i++)
+        {
+            vector_x[i] -= k * tr_vector_A[i];
+        }
+
+        iteration++;
+        if (fabs(dot_product(result_vector, result_vector, n)) >= epsilon && iteration == limit)
+        {
+            *error = ITERATION_LIMIT_EXEEDED;
+        }
+    }
+
+    free(result_vector);
+    free(vector_Ax);
+    free(tr_vector_A);
+    free(vector_AAtr);
+}
+
+/*
+mul_matrix_vector(A, p, Ap); // Ap = A * p
         double rsold = dot_product(r, r);
         double rsnew;
 
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
         {
             alpha = rsold / dot_product(p, Ap); // Calculate the alpha parameter
-            for (int j = 0; j < n; j++) 
+            for (int j = 0; j < n; j++)
             {
                 x[j] = x[j] + alpha * p[j]; // Adjusting the solution
                 r[j] = r[j] - alpha * Ap[j];
             }
             rsnew = dot_product(r, r);
-            if (sqrt(rsnew) < epsilon) 
+            if (sqrt(rsnew) < epsilon)
             {
                 break; // The required accuracy has been achieved
             }
 
             beta = rsnew / rsold; // Calculate the beta parameter
-            for (int j = 0; j < n; j++) 
+            for (int j = 0; j < n; j++)
             {
                 p[j] = r[j] + beta * p[j]; // New search direction
             }
             rsold = rsnew;
         }
-        k++;
-    }
-
-    printf("Solution:\n");
-    for (int i = 0; i < n; i++) 
-    {
-        printf("x[%d] = %.5f\n", i, x[i]);
-    }
-    return 0;
-}
+*/
