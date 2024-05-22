9f3c078b6a9dfd8588a98bfb53fad6c3f594062b
pynyashko <pynyashkx@gmail.com>
2024-05-22 19:05:50 +0300

    добавил тесты

--- Egorov_AA/2nd_term/strings/task_3/strings.c
+++ Egorov_AA/2nd_term/strings/task_3/strings.c
@@ -1,28 +1,15 @@
 #include "strings.h"
 
-void copyWords(const char* input_filename, const char* output_filename) {
-	FILE* input_file = fopen(input_filename, "r");
-	FILE* output_file = fopen(output_filename, "w");
-
-	if (input_file == NULL) {
-		perror("Ошибка открытия входного файла");
-		return;
-	}
-	if (output_file == NULL) {
-		perror("Ошибка открытия выходного файла");
-		fclose(input_file);
-		return;
-	}
-
-	int ch;
+void copyWords(FILE* input, FILE* output, error* err) {
+	int ch, index = 0;
 	char word[256];
-	int index = 0;
+	*err = OK;
 
-	while ((ch = fgetc(input_file)) != EOF) {
+	while ((ch = fgetc(input)) != EOF) {
 		if (isspace((unsigned char)ch) || ispunct((unsigned char)ch)) {
 			if (index > 0) {
 				word[index] = '\0';
-				fprintf(output_file, "%s\n", word);
+				fprintf(output, "%s\n", word);
 				index = 0;
 			}
 		}
@@ -32,9 +19,9 @@ void copyWords(const char* input_filename, const char* output_filename) {
 	}
 	if (index > 0) {
 		word[index] = '\0';
-		fprintf(output_file, "%s\n", word);
+		fprintf(output, "%s\n", word);
 	}
 
-	fclose(input_file);
-	fclose(output_file);
+	fclose(input);
+	fclose(output);
 }
--- Egorov_AA/2nd_term/strings/task_3/strings.h
+++ Egorov_AA/2nd_term/strings/task_3/strings.h
@@ -2,4 +2,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void copyWords(const char* input_filename, const char* output_filename);
+typedef enum {
+    OK,               // Все хорошо
+    NO_SOLUTION,      // Система не определена
+    SINGULAR_MATRIX,  // Матрица вырождена
+    M_ALLOC_ERR       // Память не выделена
+}error;
+
+
+void copyWords(FILE* input, FILE* output, error* err);
