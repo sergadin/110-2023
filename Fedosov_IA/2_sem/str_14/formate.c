12a15666fb0a57ad94efbc1d168c53023fd38e53
Федосов Игорь Александрович <igor.fedosov@math.msu.ru>
2024-05-29 10:25:00 +0300

    Update formate.c

--- Fedosov_IA/2_sem/str_14/formate.c
+++ Fedosov_IA/2_sem/str_14/formate.c
@@ -3,99 +3,111 @@
 #include <string.h>
 #include "formate.h"
 
-
-//функция возвращает максимальную длину строки в файле
+// функция возвращает максимальную длину строки в файле
 int maxLineLengthInFile(const char* filename) {
     FILE *file;
-    char *buffer = NULL;
-    size_t buffer_size = 0;
-    size_t read;
-    int max_length = 0;
-
-    file = fopen(filename, "r");
-
-    while ((read = getline(&buffer, &buffer_size, file)) != -1) {
-        if (read > max_length) {
-            max_length = read;
-        }
-    }
-
-    fclose(file);
-    free(buffer);
-
-    return max_length;
-}
-
-
-//Функция возвращает длину самого длинного слова в файле
-int maxWordLengthInFile(const char* filename) {
-    FILE *file;
-    char *word = NULL;
-    size_t word_size = 0;
-    size_t read;
-    int max_length = 0;
-
     file = fopen(filename, "r");
     if (file == NULL) {
         return -1;
     }
 
-    while ((read = getline(&word, &word_size, file)) != -1) {
-        char *token = strtok(word, " \n"); // Разбиваем строку на слова
-        while (token != NULL) {
-            int length = strlen(token);
+    int max_length = 0;
+    int length = 0;
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
             if (length > max_length) {
                 max_length = length;
             }
-            token = strtok(NULL, " \n");
+            length = 0;
+        } else {
+            length++;
         }
+    }
+    // Проверка последней строки, если нет завершающего \n
+    if (length > max_length) {
+        max_length = length;
     }
 
     fclose(file);
-    free(word);
-
     return max_length;
 }
 
-
-//функция считывает количество строк в файле
-int countLinesInFile(const char* filename, int max_length) {
+// Функция возвращает длину самого длинного слова в файле
+int maxWordLengthInFile(const char* filename) {
     FILE *file;
-    int count = 0;
-    char buffer[1000]; 
-
     file = fopen(filename, "r");
     if (file == NULL) {
         return -1;
     }
 
-    
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        count++; 
+    int max_length = 0;
+    int length = 0;
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == ' ' || ch == '\n' || ch == '\t') {
+            if (length > max_length) {
+                max_length = length;
+            }
+            length = 0;
+        } else {
+            length++;
+        }
+    }
+    // Проверка последнего слова, если нет завершающего разделителя
+    if (length > max_length) {
+        max_length = length;
     }
 
     fclose(file);
+    return max_length;
+}
 
+// функция считывает количество строк в файле
+int countLinesInFile(const char* filename) {
+    FILE *file;
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return -1;
+    }
+
+    int count = 0;
+    char ch;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
+    }
+    // Если файл не заканчивается новой строкой, нужно добавить одну строку
+    fseek(file, -1, SEEK_END);
+    if (fgetc(file) != '\n') {
+        count++;
+    }
+
+    fclose(file);
     return count;
 }
 
-//функция форматирует получившийся абзац
+// функция форматирует получившийся абзац
 void formatParagraph(char paragraph[], int width, FILE* outp) {
     int first_line = 1;
     char *word;
     int remaining_width = width;
-    
+    fprintf(outp, "    ");
     word = strtok(paragraph, " \n");
     while (word != NULL) {
+        int word_length = strlen(word);
         if (first_line) {
             fprintf(outp, "    ");
             remaining_width -= 4;
             first_line = 0;
         }
-        int word_length = strlen(word);
         if (word_length > remaining_width) {
-            fprintf(outp, "\n");
-            remaining_width = width;
+            fprintf(outp, "\n    ");
+            remaining_width = width - 4;
         }
         fprintf(outp, "%s ", word);
         remaining_width -= word_length + 1;
@@ -104,56 +116,55 @@ void formatParagraph(char paragraph[], int width, FILE* outp) {
     fprintf(outp, "\n");
 }
 
-//основная функция
+// основная функция
 int formate(const char* filenameA, const char* fout, int width, Error *error) {
     FILE *file, *outp;
-    char* buffer;
-    char* paragraph;
-    int max_lines;
-    int max_word_length;
-    int max_line_length;
+    char *buffer = NULL;
+    size_t buffer_size = 0;
+    size_t read;
+    char *paragraph = NULL;
+    size_t paragraph_size = 0;
 
-    // Открываем файл для чтения
     file = fopen(filenameA, "r");
     if (file == NULL) {
         *error = INP;
         return 1;
     }
+
     outp = fopen(fout, "w");
     if (outp == NULL) {
+        fclose(file);
         *error = OUTP;
         return 1;
     }
 
-    max_line_length = maxLineLengthInFile(filenameA);
-    max_word_length = maxWordLengthInFile(filenameA);
-    buffer = (char*)malloc(max_word_length * sizeof(char));
+    paragraph = (char *)malloc(1);
+    paragraph[0] = '\0';
 
-    max_lines = countLinesInFile(filenameA, max_word_length);
-
-    paragraph = (char*)malloc(max_lines * max_line_length * sizeof(char));
-
-    // Считываем текст из файла
-    paragraph[0] = '\0'; // Обнуляем строку абзаца
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        if (strcmp(buffer, "\n") == 0) { // Если встречаем пустую строку
-            if (strlen(paragraph) > 0) { // Если абзац не пустой
-                formatParagraph(paragraph, width, outp); // Форматируем абзац
-                paragraph[0] = '\0'; // Обнуляем строку абзаца для следующего абзаца
+    while ((read = getline(&buffer, &buffer_size, file)) != -1) {
+        if (strcmp(buffer, "\n") == 0) {  // Пустая строка - конец абзаца
+            if (strlen(paragraph) > 0) {
+                formatParagraph(paragraph, width, outp);
+                free(paragraph);
+                paragraph = (char *)malloc(1);
+                paragraph[0] = '\0';
                 fprintf(outp, "\n");
             }
         } else {
-            strcat(paragraph, buffer); // Добавляем строку к текущему абзацу
+            size_t new_size = strlen(paragraph) + strlen(buffer) + 1;
+            paragraph = (char *)realloc(paragraph, new_size);
+            strcat(paragraph, buffer);
         }
     }
-    if (strlen(paragraph) > 0) { // Форматируем последний абзац, если он есть
+
+    if (strlen(paragraph) > 0) {  // Форматируем последний абзац, если он есть
         formatParagraph(paragraph, width, outp);
     }
 
-    fclose(file);
-    fclose(outp);
     free(paragraph);
     free(buffer);
+    fclose(file);
+    fclose(outp);
 
     *error = OK;
     return 0;
