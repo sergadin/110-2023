f3cc06ccdf67a2da6932b7750e96ac524aa40bee
Федосов Игорь Александрович <igor.fedosov@math.msu.ru>
2024-05-29 09:27:31 +0300

    Update find_difference.c

--- Fedosov_IA/2_sem/str_7/find_difference.c
+++ Fedosov_IA/2_sem/str_7/find_difference.c
@@ -3,26 +3,30 @@
 #include <string.h>
 #include "find_difference.h"
 
-
-int max(int x, int y) {
-    if (x > y) {
+int max(int x, int y)
+{
+    if (x > y)
+    {
         return x;
-    } 
+    }
     return y;
 }
 
-int countLinesInFile(const char* filename, int max_length) {
+int countLinesInFile(const char *filename, int max_length)
+{
     FILE *file;
     int count = 0;
-    char* buffer = (char*)malloc(max_length * sizeof(char));
+    char *buffer = (char *)malloc(max_length * sizeof(char));
 
     file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return -1;
     }
 
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        count++; 
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        count++;
     }
 
     fclose(file);
@@ -31,9 +35,8 @@ int countLinesInFile(const char* filename, int max_length) {
     return count;
 }
 
-
-
-int maxLineLengthInFile(const char* filename) {
+int maxLineLengthInFile(const char *filename)
+{
     FILE *file;
     char *buffer = NULL;
     size_t buffer_size = 0;
@@ -41,12 +44,15 @@ int maxLineLengthInFile(const char* filename) {
     int max_length = 0;
 
     file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return -1;
     }
 
-    while ((read = getline(&buffer, &buffer_size, file)) != -1) {
-        if (read > max_length) {
+    while ((read = getline(&buffer, &buffer_size, file)) != -1)
+    {
+        if (read > max_length)
+        {
             max_length = read;
         }
     }
@@ -58,14 +64,17 @@ int maxLineLengthInFile(const char* filename) {
 }
 
 // Функция для считывания строк из файла
-int readLinesFromFile(const char* filename, char** lines, int max_length, int max_lines, Error *error) {
+int readLinesFromFile(const char *filename, char **lines, int max_length, int max_lines)
+{
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return -1;
     }
 
     int lineCount = 0;
-    while (fgets(lines[lineCount], max_length, file) != NULL && lineCount < max_lines) {
+    while (fgets(lines[lineCount], max_length, file) != NULL && lineCount < max_lines)
+    {
         lineCount++;
     }
 
@@ -74,61 +83,75 @@ int readLinesFromFile(const char* filename, char** lines, int max_length, int ma
 }
 
 // Функция для сравнения строк
-int compareLines(char lineA[], char lineB[]) {
+int compareLines(char lineA[], char lineB[])
+{
     return strcmp(lineA, lineB) == 0;
 }
 
-int find_difference(const char* filenameA, const char* filenameB, const char* fout, Error *error) {
-    if (maxLineLengthInFile(filenameA) == -1 || maxLineLengthInFile(filenameB) == -1) {
+int find_difference(const char *filenameA, const char *filenameB, const char *fout, Error *error)
+{
+    if (maxLineLengthInFile(filenameA) == -1 || maxLineLengthInFile(filenameB) == -1)
+    {
         *error = INP;
         return 1;
     }
     int max_length = max(maxLineLengthInFile(filenameA), maxLineLengthInFile(filenameB));
-    
+
     int max_lines = max(countLinesInFile(filenameA, max_length), countLinesInFile(filenameB, max_length));
-    char** linesA = (char**)malloc(max_lines * sizeof(char*));
-    char** linesB = (char**)malloc(max_lines * sizeof(char*));
-    for (int i = 0; i < max_lines; i++) {
-        linesA[i] = (char*)malloc(max_length* sizeof(char));
-        linesB[i] = (char*)malloc(max_length * sizeof(char));
+    char **linesA = (char **)malloc(max_lines * sizeof(char *));
+    char **linesB = (char **)malloc(max_lines * sizeof(char *));
+    for (int i = 0; i < max_lines; i++)
+    {
+        linesA[i] = (char *)malloc(max_length * sizeof(char));
+        linesB[i] = (char *)malloc(max_length * sizeof(char));
     }
-    FILE* output = fopen(fout, "w");
-    if (output == NULL) {
+    FILE *output = fopen(fout, "w");
+    if (output == NULL)
+    {
         *error = OUTP;
         return 1;
     }
 
-    int lineCountA = readLinesFromFile(filenameA, linesA, max_length, max_lines, &error);
-    int lineCountB = readLinesFromFile(filenameB, linesB, max_length, max_lines, &error);
-    
-    fprintf(output,"Строки, которые были удалены из файла A:\n");
-    for (int i = 0; i < lineCountA; i++) {
+    int lineCountA = readLinesFromFile(filenameA, linesA, max_length, max_lines);
+    int lineCountB = readLinesFromFile(filenameB, linesB, max_length, max_lines);
+
+    fprintf(output, "Строки, которые были удалены из файла A:\n");
+    for (int i = 0; i < lineCountA; i++)
+    {
         int found = 0;
-        for (int j = 0; j < lineCountB; j++) {
-            if (compareLines(linesA[i], linesB[j])) {
+        for (int j = 0; j < lineCountB; j++)
+        {
+            if (compareLines(linesA[i], linesB[j]))
+            {
                 found = 1;
                 break;
             }
         }
-        if (!found) {
+        if (!found)
+        {
             fprintf(output, "- %s", linesA[i]);
         }
     }
 
     fprintf(output, "\nСтроки, которые были добавлены в файл A:\n");
-    for (int i = 0; i < lineCountB; i++) {
+    for (int i = 0; i < lineCountB; i++)
+    {
         int found = 0;
-        for (int j = 0; j < lineCountA; j++) {
-            if (compareLines(linesB[i], linesA[j])) {
+        for (int j = 0; j < lineCountA; j++)
+        {
+            if (compareLines(linesB[i], linesA[j]))
+            {
                 found = 1;
                 break;
             }
         }
-        if (!found) {
+        if (!found)
+        {
             fprintf(output, "+ %s", linesB[i]);
         }
     }
-    for(int i = 0; i < max_lines; i++) {
+    for (int i = 0; i < max_lines; i++)
+    {
         free(linesA[i]);
         free(linesB[i]);
     }
@@ -136,5 +159,4 @@ int find_difference(const char* filenameA, const char* filenameB, const char* fo
     fclose(output);
     free(linesA);
     free(linesB);
-
 }
