7530a579aa2d41d398f24523a766b75e8fc8e603
pynyashko <pynyashkx@gmail.com>
2024-05-29 11:01:19 +0300

    добавил динамическое выделение памяти

--- Egorov_AA/2nd_term/strings/task_3/strings.c
+++ Egorov_AA/2nd_term/strings/task_3/strings.c
@@ -1,14 +1,13 @@
 #include "strings.h"
-#define MAX_WORD_LENGTH 512
 
 typedef struct Node {
-	char word[MAX_WORD_LENGTH];
+	char* word;
 	struct Node* next;
 } Node;
 
-static int addWord(Node** head, const char* word);         // Функция для добавления слова в связный список
+static int addWord(Node** head, const char* word, error* err);         // Функция для добавления слова в связный список
 
-static int addWord(Node** head, const char* word) {
+static int addWord(Node** head, const char* word, error* err) {
 	Node* current = *head;
 	while (current != NULL) {
 		if (strcmp(current->word, word) == 0) {     // Проверка на наличие слова в списке
@@ -16,12 +15,13 @@ static int addWord(Node** head, const char* word) {
 		}
 		current = current->next;
 	}
+
 	Node* new_node = (Node*)malloc(sizeof(Node));
 	if (new_node == NULL) {
-		perror("Ошибка выделения памяти");
 		exit(-1);
 	}
-	strcpy(new_node->word, word);
+
+	new_node->word = _strdup(word);
 	new_node->next = *head;
 	*head = new_node;
 	return 1;
@@ -29,40 +29,47 @@ static int addWord(Node** head, const char* word) {
 
 
 void copyWords(FILE* input, FILE* output, error* err) {
-	int ch, index = 0;
-	char word[MAX_WORD_LENGTH + 1];
+	int ch, index = 0, buffer_size = 512;
 	Node* head = NULL;
 	*err = OK;
+	char* word = (char*)malloc(buffer_size * sizeof(char));
 
 	while ((ch = fgetc(input)) != EOF) {
 		if (isspace((unsigned char)ch) || ispunct((unsigned char)ch)) {        // Если знак пункутации => слово закончилось
 			if (index > 0) {
-				word[index] = '\0';	
-				if (addWord(&head, word)) {                            // Проверяем находится ли оно в списке, если нет, то выписываем
+				word[index] = '\0';
+				if (addWord(&head, word, err)) {                            // Проверяем находится ли оно в списке, если нет, то выписываем
 					fprintf(output, "%s\n", word);
 				}
 				index = 0;
 			}
 		}
 		else {
-			if (index < MAX_WORD_LENGTH) {
-				word[index++] = (char)ch;
+			if (index >= buffer_size - 1) {
+				buffer_size *= 2;
+				char* new_word = (char*)realloc(word, buffer_size * sizeof(char));
+				if (new_word == NULL) {
+					*err = M_ALLOC_ERR;
+					free(word);
+					return;
+				}
+				word = new_word;
 			}
-			else {
-				*err = TOO_LONG_WORD;
-				return;
-			}
+			word[index++] = (char)ch;
 		}
 	}
 	if (index > 0) {                                                                // Если после последнего слова нет знака пунктуации
 		word[index] = '\0';
-		if (addWord(&head, word)) {
+		if (addWord(&head, word, err)) {
 			fprintf(output, "%s\n", word);
 		}
 	}
 
+	free(word);
+
 	while (head != NULL) {
 		Node* next = head->next;
+		free(head->word);
 		free(head);
 		head = next;
 	}
