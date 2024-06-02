44844f70f5df340407b44eb9d46aef16435c2b64
MishaGrishkin15 <143871147+MishaGrishkin15@users.noreply.github.com>
2024-06-02 22:39:04 +0300

    Add files via upload

---
+++ Grishkin_MD/find_root.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double find_root(double (*f)(double x), double a, double b, double eps);
+
---
+++ Grishkin_MD/main.c
@@ -0,0 +1,32 @@
+#include "find_root.h"
+
+double test_function (double x)
+{
+	return (x*x-15*x -100);
+}
+
+int main(void)
+{
+	double a = 4, b = 100, eps = 0.00001, res = 20, root;
+
+	double (*f)(double x) = test_function;
+	if( a > b){
+        printf("incorrect interval");
+        return -1;
+	}
+	if(f(a)*f(b) > 0){
+            printf("in this interval not 1 root");
+        return -1;
+	}
+	root = find_root(f, a, b, eps);
+	printf(" root: %lf \n", root);
+
+	if(fabs(res - root) / 5 < eps ){
+        printf("test proiden ");
+	}
+	else{
+        printf("test ne proiden");
+	}
+
+	return 0;
+}
