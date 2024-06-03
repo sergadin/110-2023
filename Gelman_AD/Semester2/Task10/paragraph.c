c1fc8613bfe953383ed743beac0700fe4dce8f0a
alinagelman <lina_art_raccon@mail.ru>
2024-06-03 15:25:20 +0300

    10
    Have done wrong task

--- Gelman_AD/Semester2/Task10/paragraph.c
+++ Gelman_AD/Semester2/Task10/paragraph.c
@@ -5,60 +5,150 @@
 #include "paragraph.h"
 
 /*Parameters:
- *         *f - The original file.
- *         *fout - Modified file.
- *         line - The number of the strings.
- *         *err - Error code.
+ *         input - The original file
+ *         output - Modified file
+ *         err - Error code
  */
 
-#define MAX_LINE_LENGTH 80 // Maximum line length
-#define RED_LINE_LENGTH 20 // Red line length
-
-// Function to display a paragraph with given borders and a red line
-void format_paragraph(char* paragraph, Error* err)
+typedef struct Define 
 {
-    char* word;
-    int remaining_space = MAX_LINE_LENGTH; // Remaining space in line
-    int is_first_word = 1; // Flag to define the first word
-    err = OK;
+	char* key;
+	char* value;
+	struct Define* next;
+} Define;
 
-    // Splitting a string into words
-    word = strtok(paragraph, " ");
-    while (word != NULL) 
-    {
-        int word_length = strlen(word);
+// Function adding define to the list
+const void add_define(Define** head, const char* key, const char* value);
+const void add_define(Define** head, const char* key, const char* value) 
+{
+	Define* new_define = (Define*)malloc(sizeof(Define));
+	new_define->key = strdup(key);
+	new_define->value = strdup(value);
+	new_define->next = *head;
+	*head = new_define;
+}
 
-        // If the word fits into the remaining space, add it
-        if (word_length <= remaining_space) 
-        {
-            if (is_first_word) 
-            {
-                printf("%s", word); // First word without space
-                remaining_space -= word_length;
-                is_first_word = 0;
-            }
-            else 
-            {
-                printf(" %s", word); // The following words with a leading space
-                remaining_space -= (word_length + 1); // Add 1 for space
-            }
-        }
-        else 
-        {
-            // If the word does not fit, go to a new line and add a red line
-            printf("\n");
-            for (int i = 0; i < RED_LINE_LENGTH; i++) 
-            {
-                printf("-");
-            }
-            printf("\n");
-            printf("%s", word);
-            remaining_space = MAX_LINE_LENGTH - word_length;
-            is_first_word = 0;
-        }
+// Searching define on the list
+const char* find_define(Define* head, const char* key);
+const char* find_define(Define* head, const char* key) 
+{
+	Define* current = head;
+	while (current != NULL) 
+	{
+		if (strcmp(current->key, key) == 0) 
+		{
+			return current->value;
+		}
+		current = current->next;
+	}
+	return NULL;
+}
 
-        word = strtok(NULL, " "); // We get the next word
-    }
+// Removing the specified define
+const void remove_define(Define** head, const char* key);
+const void remove_define(Define** head, const char* key) 
+{
+	Define* current = *head;
+	Define* prev = NULL;
 
-    printf("\n"); // Go to new line after paragraph
+	while (current != NULL) 
+	{
+		if (strcmp(current->key, key) == 0) 
+		{
+			if (prev == NULL) 
+			{
+				*head = current->next;
+			}
+			else 
+			{
+				prev->next = current->next;
+			}
+			free(current->key);
+			free(current->value);
+			free(current);
+			return;
+		}
+		prev = current;
+		current = current->next;
+	}
+}
+
+// Clearing the list (removing all the defines)
+const void free_defines(Define* head);
+const void free_defines(Define* head) 
+{
+	Define* current = head;
+
+	while (current != NULL) 
+	{
+		Define* temp = current;
+		current = current->next;
+		free(temp->key);
+		free(temp->value);
+		free(temp);
+	}
+}
+
+void process_file(FILE* input, FILE* output, error* err) 
+{
+	int buffer_size = 1024;
+	size_t length = 0;
+	ssize_t read;
+	Define* defines = NULL;
+	*err = OK;
+	char delimiters[] = " \n\t";
+	char* line = NULL;
+
+	while ((read = getline(&line, &length, input)) != -1) 
+	{
+		if (strncmp(line, "#define", 7) == 0) // If there is a line with a define
+		{
+			char* key = strtok(line + 8, " ");
+			char* value = strtok(NULL, "\n");
+
+			// Adding the define to the list
+			if (key && value) 
+			{
+				add_define(&defines, key, value);
+			}
+		}
+
+		else if (strncmp(line, "#undef", 6) == 0) // If there is a line with an undefine
+		{
+			char* key = strtok(line + 7, "\n");
+
+			// Removing the specified define
+			if (key) 
+			{
+				remove_define(&defines, key);
+			}
+		}
+
+		else // If there is a line without the define and without the undefine
+		{
+			char* token = strtok(line, delimiters); // Dividing the line into words
+
+			while (token != NULL) 
+			{
+				const char* replacement = find_define(defines, token);
+
+				if (replacement) // If the word is on the list of defines, write out a replacement
+				{
+					fprintf(output, "%s ", replacement);
+				}
+
+				else // If the word is not on the list, write out the word itself
+				{
+					fprintf(output, "%s ", token);
+				}
+				token = strtok(NULL, " \t\n");
+			}
+			fprintf(output, "\n");
+		}
+	}
+	free_defines(defines);
+	if (line) 
+	{
+		free(line);
+	}
 }
