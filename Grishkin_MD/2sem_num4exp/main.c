43e00b2d0f890eb18bf054f6da9e84cd884cf092
MishaGrishkin15 <mikhalich1337@gmail.com>
2024-06-03 10:21:12 +0300

    1

---
+++ Grishkin_MD/2sem_num4exp/main.c
@@ -0,0 +1,26 @@
+#include "taylor_exp.h"
+
+int main(void)
+{
+    double eps = 0.0001, x = 5, expected = 148.413159103, res;
+	res = taylor_exp(x, eps);
+
+	if(error_code == LIM)
+    {
+        printf("the iteration limit has been exceeded\n");
+        return -1;
+	}
+    if(error_code == OK)
+    {
+	    printf("%lf\n", res);
+	    if(fabs(res - expected) < eps)
+	    {
+	        printf("test proiden");
+	    }
+        else
+        {
+            printf("test ne proiden");
+        }
+    }
+    return 0;
+}
---
+++ Grishkin_MD/2sem_num4exp/taylor_exp.c
@@ -0,0 +1,36 @@
+#include "taylor_exp.h"
+
+double factorial(double x)
+{
+	double n = 1, ans = 1;
+	while (n<=x)
+	{
+		ans = ans * n;
+		n++;
+	}
+	return ans;
+}
+
+double taylor_exp(double x, double eps)
+{
+   double ans = 1, last_ans = 0, n = 1;
+   int limit = 10000;
+
+
+	while (fabs(last_ans - ans) > eps)
+	{
+		last_ans = ans;
+		limit--;
+
+		ans += (pow(x,n)/factorial(n));
+		n++;
+	}
+	if(limit < 0)
+    {
+        error_code = LIM;
+	}
+	error_code = OK;
+
+	//printf("%lf \n", n);
+    return ans;
+}
