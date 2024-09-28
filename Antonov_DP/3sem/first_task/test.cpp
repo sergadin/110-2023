9e35e9990227ee6694e04bceaf6fa0236aeb2e3e
solaire758 <144099475+solaire758@users.noreply.github.com>
2024-09-28 19:18:34 +0300

    Update test.cpp обновлён тест

--- Antonov_DP/3sem/first_task/test.cpp
+++ Antonov_DP/3sem/first_task/test.cpp
@@ -2,21 +2,25 @@
 #include "intset.h"
 
 int main() {
-  intset big(0, 100);
-  const int max_iterations = 500;
+    intset big(0, 100);
+    const int max_iterations = 500;
 
-  for(int iter = 0; iter < max_iterations; iter++) {
-    intset temp(0, big.right());
-    for(int item = temp.left(); item <= temp.right(); item++) {
-      if((item % 7) > (iter % 11))
-        temp.add(item);
+    for (int iter = 0; iter < max_iterations; iter++) {
+        intset temp(0, big.right());
+        for (int item = temp.left(); item <= temp.right(); item++) {
+            if ((item % 7) > (iter % 11)) {
+                temp.add(item);
+            }
+        }
+        if (iter > 0) {
+            big = big * temp;
+            std::cout << big.max() << "\n";
+        }
+        else {
+            big = temp;
+        }
     }
-    if(iter > 0)
-      big = big * temp;
-    else
-      big = temp;
-  }
-  big = big;
-  std::cout << "min = " << big.min() << ", max = " << big.max() << std::endl;
-  return 0;
+    big = big;
+    std::cout << "min = " << big.min() << ", max = " << big.max() << std::endl;
+    return 0;
 }
