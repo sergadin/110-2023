9f8cdaff75523af2de12f6833a0a307b5390f185
solaire758 <144099475+solaire758@users.noreply.github.com>
2024-09-28 19:18:57 +0300

    Update intset.cpp подправлен код

--- Antonov_DP/3sem/first_task/intset.cpp
+++ Antonov_DP/3sem/first_task/intset.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include "intset.h"
 
-intset::intset(int left, int right){
-	if (left > right){
+intset::intset(int left, int right) {
+	if (left > right) {
 		throw Intset_Exception(-1, "left > right");
 	}
 	left_ = left;
 	right_ = right;
 	set_ = new int[right - left + 1];
-	for (int i = 0; i < right - left + 1; i++){
+	for (int i = 0; i < right - left + 1; i++) {
 		set_[i] = left - 1;
 	}
 }
 
-intset::intset(const intset &other){
+intset::intset(const intset& other) {
 	left_ = other.left_;
 	right_ = other.right_;
 	set_ = new int[right_ - left_ + 1];
@@ -23,135 +23,134 @@ intset::intset(const intset &other){
 	}
 }
 
-intset::~intset(){
+intset::~intset() {
 	left_ = 0;
 	right_ = 0;
 	delete[] set_;
 }
 
-void intset::add(const int elem){
-	if ((elem > right_) || (elem < left_)){
-		throw Intset_Exception(-2, "elem not in range of set");
+void intset::add(const int elem) {
+	if ((elem > right_) || (elem < left_)) {
+		throw Intset_Exception(-1, "elem not in range of set");
 	}
-	if (set_[elem - left_] != (left_ - 1)){
+	if (set_[elem - left_] != (left_ - 1)) {
 		return;
 	}
 	set_[elem - left_] = elem;
 }
 
-void intset::del(const int elem){
-	if ((elem > right_) || (elem < left_)){
-                throw Intset_Exception(-2, "elem not in range of set");
-        }
-	if (set_[elem - left_] != (left_ - 1)){
+void intset::del(const int elem) {
+	if ((elem > right_) || (elem < left_)) {
+		throw Intset_Exception(-1, "elem not in range of set");
+	}
+	if (set_[elem - left_] != (left_ - 1)) {
 		set_[elem - left_] = (left_ - 1);
-        }
+	}
 }
 
-bool intset::check(){
+bool intset::check() {
 	for (int i = 0; i < right_ - left_ + 1; i++)
 	{
-		if (set_[i] != (left_ - 1)){
+		if (set_[i] != (left_ - 1)) {
 			return true;
 		}
 	}
 	return false;
 }
 
-int intset::len(){
+int intset::len() {
 	int len = 0;
 	for (int i = 0; i < right_ - left_ + 1; i++)
-        {
-                if (set_[i] != (left_ - 1)){
-                        len++;
-                }
-        }
+	{
+		if (set_[i] != (left_ - 1)) {
+			len++;
+		}
+	}
 	return len;
 }
 
-int intset::max(){
-	if (!(this->check())){
-		throw Intset_Exception(-3, "set is empty");
+int intset::max() {
+	if (!(this->check())) {
+		return right_ + 1;
 	}
 	for (int i = right_ - left_; i > -1; i--)
-        {
-                if (set_[i] != (left_ - 1)){
-                        return set_[i];
-                }
-        }
+	{
+		if (set_[i] != (left_ - 1)) {
+			return set_[i];
+		}
+	}
 	return 0;
 }
 
-int intset::min(){
-        if (!(this->check())){
-                throw Intset_Exception(-3, "set is empty");
-        }
-        for (int i = 0; i < right_ - left_ + 1; i++)
-        {
-                if (set_[i] != (left_ - 1)){
-                        return set_[i];
-                }
-        }
-        return 0;
+int intset::min() {
+	if (!(this->check())) {
+		return left_ - 1;;
+	}
+	for (int i = 0; i < right_ - left_ + 1; i++)
+	{
+		if (set_[i] != (left_ - 1)) {
+			return set_[i];
+		}
+	}
+	return 0;
 }
 
-int intset::left(){
-        return left_;
+int intset::left() {
+	return left_;
 }
 
-int intset::right(){
-        return right_;
+int intset::right() {
+	return right_;
 }
 
-intset &intset::operator=(const intset &other){
-	if (*this == &other){
-		exit(-1);
-}
-        left_ = other.left_;
-        right_ = other.right_;
-        set_ = new int[right_ - left_ + 1];
-        for (int i = 0; i < right_ - left_ + 1; i++)
-        {
-                set_[i] = other.set_[i];
-        }
+intset& intset::operator=(const intset& other) {
+	if (this == &other) {
+		return *this;
+	}
+	left_ = other.left_;
+	right_ = other.right_;
+	set_ = new int[right_ - left_ + 1];
+	for (int i = 0; i < right_ - left_ + 1; i++)
+	{
+		set_[i] = other.set_[i];
+	}
 	return *this;
 }
 
-intset operator*(const intset &other1, const intset &other2){
+intset operator*(const intset& other1, const intset& other2) {
 	intset temp(other1.left_, other1.right_);
 	if ((other1.right_ < other2.left_) || (other1.left_ > other2.right_))
 	{
-		throw Int_Exception(-4, "not intersect");
+		throw Intset_Exception(-1, "not intersect");
 	}
-	if(other1.left_ < other2.left_){
+	if (other1.left_ < other2.left_) {
 		temp.left_ = other2.left_;
- }
-	if(other1.right_ > other2.right_){
-                temp.right_ = other2.right_;
-        }
-	for (int i = 0; i < temp.right_ - temp.left_ + 1; i++){
-		if (other1.set_[i] == other2.set_[i]){
+	}
+	if (other1.right_ > other2.right_) {
+		temp.right_ = other2.right_;
+	}
+	for (int i = 0; i < temp.right_ - temp.left_ + 1; i++) {
+		if (other1.set_[i] == other2.set_[i]) {
 			temp.set_[i] = other1.set_[i];
 		}
-		else{
+		else {
 			temp.set_[i] = temp.left_ - 1;
 		}
 	}
 	return temp;
 }
 
-bool operator==(const intset &other1, const intset &other2){
-	if (other1.left_ != other2.left_){
+bool operator==(const intset& other1, const intset& other2) {
+	if (other1.left_ != other2.left_) {
 		return false;
 	}
-	if (other1.right_ != other2.right_){
-                return false;
-        }
-	for (int i = 0; i < other1.right_ - other1.lefr_ + 1; i++){
-                if (other1.set_[i] != other2.set_[i]){
-                        return false;
-                }
-        }
+	if (other1.right_ != other2.right_) {
+		return false;
+	}
+	for (int i = 0; i < other1.right_ - other1.left_ + 1; i++) {
+		if (other1.set_[i] != other2.set_[i]) {
+			return false;
+		}
+	}
 	return true;
 }
-
