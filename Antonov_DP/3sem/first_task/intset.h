7829e578a857097a6d11c3296b4fe81dc283189b
solaire758 <144099475+solaire758@users.noreply.github.com>
2024-10-06 11:31:23 +0300

    добавлено больше const

--- Antonov_DP/3sem/first_task/intset.h
+++ Antonov_DP/3sem/first_task/intset.h
@@ -1,44 +1,44 @@
 #include <iostream>
 
-class intset{
-	int *set_;
+class intset {
+	int* set_;
 	int left_;
 	int right_;
 public:
 	intset() = delete;
 	// конструктор работает только с двумя входными данными, без входных данных будет ошибка
 	intset(int left, int right);
-	intset(const intset &other);
+	intset(const intset& other);
 	~intset();
 	// добавляет элемент, если элемент за пределами множества, то будет исключительная ситуация с -1 Intset_Exception
 	void add(const int elem);
 	// удаляет элемент, если элемент за пределами множества, то будет исключительная ситуация с -1 Intset_Exception
 	void del(const int elem);
 	// проверяет пусто ли множество
-	bool check();
+	bool check() const;
 	// выдаёт длину множества
-	int len();
+	int len() const;
 	// выводит максимальное число в множестве, но если множество пусто то выводит right_ + 1
-	int max();
+	int max() const;
 	// выводит минимальное число в множестве, но если множество пусто то выводит left_ - 1
-	int min();
+	int min() const;
 	// выдаёт левую границу
-	int left();
+	int left() const;
 	// выдаёт правую границу
-	int right();
-	intset &operator=(const intset &other);
+	int right() const;
+	intset& operator=(const intset& other);
 	// если отрезки не пересекаются то выдаёт пустое множество с границами left_ левого и right_ правого
 	// иначе честное пересечение множество с границами пересечения
-	friend intset operator*(const intset &other1, const intset &other2);
-	friend intset operator*=(intset &ours, const intset &other2);
-	friend bool operator==(const intset &other1, const intset &other2);
+	friend intset operator*(const intset& other1, const intset& other2);
+	friend intset operator*=(intset& ours, const intset& other2);
+	friend bool operator==(const intset& other1, const intset& other2);
 };
 
 class Intset_Exception {
-  int code_;
-  std::string message_;
+	int code_;
+	std::string message_;
 public:
-  Intset_Exception(int code, const std::string& message) : code_(code), message_(message) {}
-  const std::string& message() const { return message_; }
-  int code() const { return code_; }
+	Intset_Exception(int code, const std::string& message) : code_(code), message_(message) {}
+	const std::string& message() const { return message_; }
+	int code() const { return code_; }
 };
