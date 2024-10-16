020b345cc58c18c431b51f853b9c4890d4652d22
MishaGrishkin15 <143871147+MishaGrishkin15@users.noreply.github.com>
2024-10-16 16:24:20 +0300

    Update main.c

--- Grishkin_MD/2sem_5.3/main.c
+++ Grishkin_MD/2sem_5.3/main.c
@@ -1,8 +1,32 @@
 #include "interpolation.h"
 
+void write_points(FILE* out1, FILE* out2, point* p, int n, double* i_p, int m, double* res);
+
+void write_points(FILE* out1, FILE* out2, point* p, int n, double* i_p, int m, double* res){
+	for (int i = 0; i < n; i++){
+		fprintf(out1, "%lf\t%lf\n", p[i].x, p[i].y);
+	}
+	for (int i = 0; i < m; i++){
+		fprintf(out2, "%lf\t%lf\n", i_p[i], res[i]);
+	}
+}
+
 
 int main(void) {
     double eps = 0.001;
+    char filename1[32], filename2[32];
+    FILE* out1;
+    FILE* out2;
+
+    sprintf(filename1, "out1.txt");
+    sprintf(filename2, "out2.txt" );
+    out1 = fopen(filename1, "w");
+    out2 = fopen(filename2, "w");
+
+    if (out1 == NULL || out2 == NULL) {
+        perror("Error opening file");
+        return -1;
+    }
 
     point points[5] = {
         {1, 1},
@@ -28,6 +52,8 @@ int main(void) {
             printf("The value does not match the expected value\n");
         }
     }
+    write_points(out1, out2, points, 5, interp_points, 4, interp_values);
 
     return 0;
 }
+
