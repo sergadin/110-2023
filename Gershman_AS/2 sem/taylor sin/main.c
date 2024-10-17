07c591b76ecac88bc0385c8ef6c878da2891e7aa
artemgershman <143938528+artemgershman@users.noreply.github.com>
2024-10-17 14:56:12 +0300

    Add files via upload

---
+++ Gershman_AS/2 sem/taylor sin/main.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include "taylor_sin.h"
+
+typedef struct {          // Структура тестов:
+	double x;             //   - Точка
+	double eps;           //   - Точность
+	double res;           //   - Ожидаемый результат
+	Error err_code;       //   - Ожидаемая ошибка
+} TestCase;
+
+int main(void) {
+	int test_num, i_num;
+	Error err;
+	double value;
+	TestCase tests[] = {
+		{-2, 0.0001, -0.03489, OK},
+		{0.9, 0.0001, 0.7833269, OK},
+		{1000, 0.0001, -0.75680249, W_ARG},
+	};
+
+
+	for (int i = 0; i < 3; i++) {                                            // Тестирование
+		i_num = 0;
+		value = taylor_sin(tests[i].x, tests[i].eps, &err);
+		if (err != tests[i].err_code) {
+			printf("Тест %d не пройден\n", i + 1);
+		}
+		else if ((err == OK) && (fabs(tests[i].res - value) / 2 > tests[i].eps)) {
+			printf("Тест %d не пройден\n", i + 1);
+		}
+		else {
+			printf("Тест %d пройден\n", i + 1);
+		}
+	}
+
+	return 0;
+}
