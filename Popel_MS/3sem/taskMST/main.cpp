8498d4b55a0e0035bfdc66bb4b0b463ad8a5e4bd
maria-popel <mariia.popel@math.msu.ru>
2024-10-18 17:44:34 +0300

    Fix main

--- Popel_MS/3sem/taskMST/main.cpp
+++ Popel_MS/3sem/taskMST/main.cpp
@@ -5,23 +5,30 @@
 using namespace std;
 using namespace shortNames;
 int main(void){
-  Points points = {{3, 2},{2, 6},{9, 1}, {14, 6}, {8, 4}, {6, 6}, {3, 2}, {11, 8}, {7, 9}, {4, 11},{12, 3}};
-  Point pnt(2, 2);
-  Point dpnt(6, 6);
-  
-  printf("first - simple\n");
-  MST_builder mst1(points);
-  vector<Edge> res = mst1.MST_build();
-  plotMst(res);
-  
-  mst1.add_point(pnt);
-  printf("second - add point\n");
-  vector<Edge> res2 = mst1.MST_build();
-  plotMst(res2);
-  
-  mst1.del_point(dpnt);
-  printf("third - delete point\n");
-  vector<Edge> res3 = mst1.MST_build();
-  plotMst(res3);
+try{
+    Points points = {{3, 2},{2, 6},{9, 1}, {14, 6}, {8, 4}, {6, 6}, {3, 2}, {11, 8}, {7, 9}, {4, 11},{12, 3}};
+    Point pnt(2, 2);
+    Point dpnt(6, 6);
+    
+    printf("first - simple\n");
+    MST_builder mst1(points);
+    vector<Edge> res = mst1.MST_build();
+    plotMst(res);
+    
+    mst1.add_point(pnt);
+    printf("second - add point\n");
+    vector<Edge> res2 = mst1.MST_build();
+    plotMst(res2);
+    
+    mst1.del_point(dpnt);
+    printf("third - delete point\n");
+    vector<Edge> res3 = mst1.MST_build();
+    plotMst(res3);
+    return 0;
+  }catch(MstError &err){
+    cout << "EXCEPTION: " << err.getReason() << endl;
+  }catch(...){
+    cout << "Something wrong" << endl;
+  }
   return 0;
 }
