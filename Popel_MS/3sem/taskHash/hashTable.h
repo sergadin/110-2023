43dfc943f49fed4af520c8de17387b2a5f3d5302
maria-popel <mariia.popel@math.msu.ru>
2024-11-02 02:04:55 +0300

    New draft

--- Popel_MS/3sem/taskHash/hashTable.h
+++ Popel_MS/3sem/taskHash/hashTable.h
@@ -1,11 +1,14 @@
+#ifndef TBL
+#define TBL
 #include <iostream>
 #include <vector>
 #include <string>
 #include <list>
+#include <random>
+#include <fstream>
 
 
-
-
+//класс ошибок
 class HashEr{
   private:
     int code_;
@@ -16,37 +19,48 @@ class HashEr{
       return code_;
     }
     std::string getMes() {
-      return mes_;
+      return mes_; 
     }
 };
+
+
 template<typename T>
 class HashTbl {
   private:
     struct HashEl{ //Структура с данными для отображения
-      std::string key;
-      T val;
-      bool ntEmpty;
+      std::string key; //ключ в формате строки
+      T val; //соответствующее ключу значение
+      bool ntEmpty; //есть или нет значения
       
-      HashEl() : ntEmpty(false){}
-      HashEl(const std::string& k, const T& v) : key(k), val(v), ntEmpty(true) {} 
+      HashEl() : key(""), val(0), ntEmpty(false){} //конструктор без элементов
+      HashEl(const std::string& k, const T& v) : key(k), val(v), ntEmpty(true) { //конструктор по ключу и значению
+        if (k.empty()) {
+          throw HashEr(-6, std::string("Key cannot be empty\n"));
+        }
+        key = k;
+        ntEmpty = true;
+      } 
     };
     
     std::vector<HashEl> table; //Вектор элементов структуры
     int curSize; //Количество хранящихся ключей
     int tblSize; //Максимальное количество ключей (количество выделенной памяти)
     
-    int hash(const std::string& k) const {
-      std::hash<std::string> hashFun;
-      return hashFun(k) % tblSize;
-    }
     
     void rehash(); //Перезапись Хэш-таблицы - увеличение памяти
     
     public:
-      HashTbl(int newCap = 10) : tblSize(newCap), curSize(0) {
-        table.resize(tblSize);
+      HashTbl(int newCap = 10) : table(newCap), curSize(0), tblSize(newCap){ //конструктор хэш-таблицы
+      if(newCap <= 0 ){
+        throw HashEr(-5, std::string("Table size must be positive\n"));
+      }
+        table.resize(newCap);
+        for (auto& el : table){
+          el.ntEmpty = false;
+        }
       }
       
+      int hash(const std::string& str) const; //хэш-функция
       void add(const std::string& key, const T& value); //Функция добавления точки
       
       T get(const std::string& key) const; //Функция получения точки
@@ -58,23 +72,24 @@ class HashTbl {
       
       class Iterator { //Итератор
         private: 
-          const HashTbl& table;
-          int ind;
+          const HashTbl& table;//хэш-таблица
+          int ind; //текущая позиция
          
          public:
-          Iterator(const HashTbl& t, int i) : table(t), ind(i) {}
+          Iterator(const HashTbl& t, int i) : table(t), ind(i) {} //конструктор
           
-          bool operator!=(const Iterator& other) const {
-            return ind!=other.ind;
+          bool operator!=(const Iterator& other) const { //оператор неравенства
+            return ind != other.ind;
           }
           
-          void operator++() {
+          Iterator& operator++() { //оператор шага (увеличения индекса на один)
             do{
               ind++;
             }while (ind < table.tblSize && !(table.table[ind].ntEmpty));
+            return *this;
           }
           
-          std::pair<std::string, T> operator*() const {
+          std::pair<std::string, T> operator*() const { //оператор-указатель (возвращает уже сам элемент по индексу)
             return {table.table[ind].key, table.table[ind].val};
           }
       };
@@ -89,4 +104,161 @@ class HashTbl {
       Iterator end() const {
         return Iterator(*this, tblSize);
       }
+
+      std::string gen_rnd_key(); //генератор случайных ключей
+      int gen_rnd_val(); //генератор случайных значений (целочисленный)
+      void gen_rnd_set(int sz); //генератор случайных множеств
+      void readFile(const std::string& filename); //чтение множества из файла
 };
+
+template <typename T>
+int HashTbl<T>::hash(const std::string& str) const{
+  int h =  0;
+  for (char c : str){
+    h = h * 31 + static_cast<unsigned char>(c);
+  }
+  return h;
+}
+
+template<typename T>
+      void HashTbl<T>::rehash() {
+      int counter = 0;
+      tblSize *= 2;
+      std::vector<HashEl> oldTable = table;
+      table = std::vector<HashEl>(tblSize);
+      curSize = 0;
+      
+      for (auto& el : table){
+        el.ntEmpty = false;
+      }
+      
+      for (int i = 0; i < tblSize; i++) {
+        if (oldTable[i].ntEmpty){
+          this->add(oldTable[i].key, oldTable[i].val);
+          table[counter].ntEmpty = true;
+          counter++;
+        }
+      }
+    }
+
+  template<typename T>
+  void HashTbl<T>::add(const std::string& key, const T& value) {
+        if (key.empty()) {
+          throw HashEr(-6, std::string("Key cannot be empty\n"));
+        }
+        if (curSize >= (tblSize / 2)){
+          this->rehash();
+        }
+        int index = hash(key)%tblSize;
+        while ((table[index].ntEmpty) && !(table[index].key.empty())) {
+          if (table[index].key == key){
+            table[index].val = value;
+            return;
+          }
+          index = (index + 1) % tblSize;
+        }
+        table[index] = HashEl(key, value);
+        table[index].ntEmpty = true;
+        curSize++;
+      }
+      
+      template<typename T>
+      T HashTbl<T>::get(const std::string& key) const {
+        int index = HashTbl::hash(key);
+        while (table[index].ntEmpty) {
+          if (table[index].key == key){
+            return table[index].val;
+          }
+          index = (index + 1) % tblSize;
+        }
+        throw HashEr(-1, std::string("Key not found\n"));
+      }
+
+template<typename T>
+void HashTbl<T>::remove(const std::string& key) {
+  int index = HashTbl::hash(key) % tblSize;
+  int nextIndex = 0;
+  while (table[index].ntEmpty) {
+    if (table[index].key == key){
+      //table[index].ntEmpty = false;
+      curSize--;
+      
+      nextIndex = (index + 1) % tblSize;
+      while ((nextIndex < tblSize) && (table[nextIndex].ntEmpty)){
+        table[index] = table[nextIndex];
+        index = nextIndex;
+        nextIndex = (nextIndex + 1) % tblSize;
+      }
+      table[index].ntEmpty = false;
+      return;
+    }
+    index = (index + 1) % tblSize;
+  }
+}
+template <typename T>
+std::string HashTbl<T>::gen_rnd_key(){
+  const std::string chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+  const int k_sz = 10;
+  
+  std::random_device rd; //генератор случайных чисел
+  std::mt19937 gen(rd()); //инициализация генератора случайных чисел
+  std::uniform_int_distribution<> dist(0, chars.size() - 1);//равномерное распределение значений
+  
+  std::string key(k_sz, ' ');
+  for (int i = 0; i < k_sz; ++i){
+    key[i] = chars[dist(gen)];
+  }
+  
+  return key;
+}
+template <typename T>
+int HashTbl<T>::gen_rnd_val(){
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dist(1, 100);
+  
+  return dist(gen);
+}
+
+template <typename T>
+void HashTbl<T>::gen_rnd_set(int sz){
+  for (int i = 0; i < sz; ++i){
+    std::string k = gen_rnd_key();
+    T v = gen_rnd_val();
+    this->add(k, v);
+  }
+}
+
+template <typename T>
+void HashTbl<T>::readFile(const std::string& filename){
+  std::ifstream input_file(filename);
+  if (!input_file.is_open()){
+    throw HashEr(-4, std::string("File openning problems\n"));
+  }
+  std::string key;
+  std::string type;
+  T val;
+  std::getline(input_file, type);
+  if (type == "std::string"){
+      while (input_file >> key >> val){
+    this->add(key, val);
+  }
+  }else if (type == "double"){
+        while (input_file >> key >> val){
+    this->add(key, val);
+  }
+  }else if (type == "int"){
+        while (input_file >> key >> val){
+    this->add(key, val);
+  }
+  }else if (type == "char"){
+        while (input_file >> key >> val){
+    this->add(key, val);
+  }
+  }else {
+  input_file.close();
+    throw HashEr(-3, std::string("This type isn't allowed\n"));
+  }
+  input_file.close();
+}
+#endif
--- Popel_MS/3sem/taskHash/main.cpp
+++ Popel_MS/3sem/taskHash/main.cpp
@@ -1,37 +1,106 @@
 #include "hashTable.h"
 #include <cstdlib>
 #include <ctime>
+#include <random>
 #include <fstream>
 #include <sstream>
-
-static void LoadFile(const std::string &filename) {
-  std::ifstream file(filename);
-  if (!file.is_open()) {
-    throw HashEr(-2, std::string("Openning problems\n"));
-  }
-  std::string key;
-  V val;
-  auto type; //как ввести тип
-  ///
-  file.close();
-}
-
-static HashTbl<V> GenRndTbl(int sz) {
-  HashTbl<V> table;
-  for (int i = 0; i < sz; ++i){
-    table.add("key" + std::to_string(i), V(i));
-  }
-  return table;
-}
-
-class ExampleClass{
+#include <cassert>
+/*
+ static double max(double x, double y, double eps);
+ 
+  static double max(double x, double y, double eps){
+    double max_1 = x;
+    if(x>y){
+      max_1 = x;
+    }else{
+      max_1 = y;
+    }
+    if (eps > max_1){
+      return eps;
+    }
+    return max_1;
+  }*/
+/*class ExampleClass{
   public:
     int number;
     
     ExampleClass(int n) : number(n) {}
+};*/
+/*template <typename T>
+struct TestCase{
+  const std::string type;
+  std::vector<std::string> key;
+  std::vector<T> exp_val;
+};
+TestCase<int> intTests[] = {
+  {"int", {"ключ1", "ключ2", "ключ3"}, {10, 20, 30}}
 };
 
+TestCase<std::string> stringTests[] = {
+  {"std::string", {"ключ1", "ключ2", "ключ3", "ключ4"}, {"abc", "names", "stack", "keys"}}
+};
+
+TestCase<double> doubleTests[] = {
+  {"double", {"ключ1", "ключ2"}, {12.4, 0.22}}
+};*/
 int main(void){
-  auto randSet = HashTbl<int>::GenRndTbl(15);
+  HashTbl<int> h1;
+  //HashTbl<std::string> h2;
+  // Проверка добавления и получения значений
+  h1.add("ключ1", 10);
+  h1.add("ключ2", 20);
+  h1.add("ключ3", 30);
+  if(h1.get("ключ1") == 10){
+    printf("Good!\n");
+  }
+  /*assert(h1.get("ключ2") == 20);
+  assert(h1.get("ключ3") == 30);
+
+  // Проверка размера хэш-таблицы
+  assert(h1.size() == 3);
+
+  // Проверка удаления элементов
+  h1.remove("ключ2");
+  assert(h1.size() == 2);
+  assert(h1.get("ключ1") == 10);
+  assert(h1.get("ключ3") == 30);
+
+  // Проверка rehash
+  h1.add("ключ4", 40);
+  h1.add("ключ5", 50);
+  h1.add("ключ6", 60);
+  assert(h1.size() == 5);
+
+  // Проверка итератора
+  std::cout << "Проверка итератора:\n";
+  for (auto it = h1.begin(); it != h1.end(); ++it) {
+    std::cout << "Ключ: " << (*it).first << ", значение: " << (*it).second << std::endl;
+  }
+
+  // Добавление строк
+  h2.add("ключ1", "значение1");
+  h2.add("ключ2", "значение2");
+  h2.add("ключ3", "значение3");
+
+  // Проверка получения значений
+  assert(h2.get("ключ1") == "значение1");
+  assert(h2.get("ключ2") == "значение2");
+  assert(h2.get("ключ3") == "значение3");
+
+  // Проверка размера
+  assert(h2.size() == 3);
+
+  // Проверка удаления
+  h2.remove("ключ2");
+  assert(h2.size() == 2);
+  assert(h2.get("ключ1") == "значение1");
+  assert(h2.get("ключ3") == "значение3");
+
+  // Проверка итератора
+  std::cout << "Проверка итератора:\n";
+  for (auto it = h2.begin(); it != h2.end(); ++it) {
+    std::cout << "Ключ: " << (*it).first << ", значение: " << (*it).second << std::endl;
+  }*/
+
   return 0;
 }
