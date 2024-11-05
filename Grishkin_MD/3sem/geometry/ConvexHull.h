ad43d9091bfc0fa7de463e79af577029244c3b73
MishaGrishkin15 <143871147+MishaGrishkin15@users.noreply.github.com>
2024-11-05 03:39:27 +0300

    Update ConvexHull.h

--- Grishkin_MD/3sem/geometry/ConvexHull.h
+++ Grishkin_MD/3sem/geometry/ConvexHull.h
@@ -6,23 +6,18 @@
 
 class ConvexHull {
 public:
-    // Конструктор
     ConvexHull();
 
-    // Добавление точки
     void addPoint(const Point& point);
 
-    // Построение выпуклой оболочки
     std::vector<Point> buildHull();
 
 private:
-    // Вспомогательные методы
     std::vector<Point> findLowerHull(const std::vector<Point>& points);
     std::vector<Point> findUpperHull(const std::vector<Point>& points);
     void removeDuplicates(std::vector<Point>& points);
 
-    // Хранилище точек
     std::vector<Point> points;
 };
 
-#endif // CONVEX_HULL_H
+#endif 
