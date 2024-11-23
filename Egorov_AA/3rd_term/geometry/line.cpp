823234bdc33fc5825bcce7bbdc6d2c0c98a2ee3a
pynyashko <143870977+pynyashko@users.noreply.github.com>
2024-11-23 08:13:16 +0300

    Update line.cpp

--- Egorov_AA/3rd_term/geometry/line.cpp
+++ Egorov_AA/3rd_term/geometry/line.cpp
@@ -1,31 +1,38 @@
+#include <cmath>
+#include <algorithm>
 #include "line.h"
-#include <stdexcept>
-#include <math.h>
 
 Line::Line(double a, double b, double c) : a(a), b(b), c(c) {}
 
 Line::Line(const Point& p1, const Point& p2) {
     a = p2.getY() - p1.getY();
     b = p1.getX() - p2.getX();
-    c = a * p1.getX() + b * p1.getY();
+    c = -(a * p1.getX() + b * p1.getY());
 }
 
-bool Line::intersects(const Line& other) const {
-    if ((fabs(a * other.b - b * other.a) - 0) > 1e-10) {
+bool Line::intersectSegment(const Point& p1, const Point& p2, Point& intersection) const {
+    double x1 = p1.getX(), y1 = p1.getY(),
+        x2 = p2.getX(), y2 = p2.getY(),
+        xi, yi;
+    double denominator = a * (x2 - x1) + b * (y2 - y1);
+    if (std::abs(denominator) == 0) 
+        return false;
+
+    double t = -(a * x1 + b * y1 + c) / denominator;
+    if (t < 0 || t > 1) 
+        return false;
+
+    xi = x1 + t * (x2 - x1);
+    yi = y1 + t * (y2 - y1);
+    
+    intersection.setX(xi);
+    intersection.setY(yi);
+
+    // Проверяем, лежит ли точка на отрезке
+    if (std::min(x1, x2) - 1e-9 <= xi && xi <= std::max(x1, x2) + 1e-9 &&
+        std::min(y1, y2) - 1e-9 <= yi && yi <= std::max(y1, y2) + 1e-9) {
         return true;
     }
+
     return false;
-}
-
-Point Line::intersection(const Line& other) const {
-    double det = a * other.b - b * other.a;
-    if (fabs(det - 0) > 1e-10) throw std::runtime_error("Линии не пересекаются.");
-    double x = (b * other.c - c * other.b) / det;
-    double y = (c * other.a - a * other.c) / det;
-    return Point(x, y);
-}
-
-void Line::draw() const {
-    // Можно выводить уравнение линии
-    std::cout << a << "x + " << b << "y + " << c << " = 0" << std::endl;
 }
