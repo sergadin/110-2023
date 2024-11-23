52e29939b300783566e61447aa0ad4597f31508b
pynyashko <143870977+pynyashko@users.noreply.github.com>
2024-11-23 08:13:30 +0300

    Update line.h

--- Egorov_AA/3rd_term/geometry/line.h
+++ Egorov_AA/3rd_term/geometry/line.h
@@ -5,20 +5,14 @@
 
 class Line {
 private:
-    double a, b, c; // Коэффициенты уравнения прямой ax + by + c = 0
+    double a, b, c; // ax + by + c = 0
 
 public:
+
     Line(double a, double b, double c);
     Line(const Point& p1, const Point& p2);
 
-    // Проверка, пересекается ли прямая с другой
-    bool intersects(const Line& other) const;
-
-    // Получение точки пересечения
-    Point intersection(const Line& other) const;
-
-    // Отрисовка линии для gnuplot
-    void draw() const;
+    bool intersectSegment(const Point& p1, const Point& p2, Point& intersection) const;
 };
 
 #endif // LINE_H
