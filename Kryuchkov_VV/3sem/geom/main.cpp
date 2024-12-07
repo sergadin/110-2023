c477cc64292c2baca189b2d57d07973a5b395f70
vllodimer <vladimir.kruchkov-kurnosenko@math.msu.ru>
2024-12-07 09:31:22 +0300

    ближе к правде

--- Kryuchkov_VV/3sem/geom/main.cpp
+++ Kryuchkov_VV/3sem/geom/main.cpp
@@ -14,13 +14,13 @@ struct Point
 class ConvexPolygon
 {
 public:
-    std::vector<Point> vertices_; // Вектор для хранения вершин многоугольника
+    std::vector<Point> vertices_;
 
     ConvexPolygon() = delete;
 
-    ConvexPolygon(const std::vector<Point>& points); // Конструктор
-    ConvexPolygon(const ConvexPolygon& other); // Конструктор копирования
-    ~ConvexPolygon(); // Деструктор
+    ConvexPolygon(const std::vector<Point>& points);
+    ConvexPolygon(const ConvexPolygon& other);
+    ~ConvexPolygon();
 
     void print() const
     {
@@ -32,94 +32,31 @@ public:
 
     static int countPoints(const ConvexPolygon& points)
     {
-        return points.vertices_.size(); // Возвращает количество элементов в векторе.
+        return points.vertices_.size();
     }
 
-    void addPoint(int x, int y) {
-        vertices_.push_back(Point(x, y));
-    }
-
-    void intersect(double a, double b, double c) {
-        vector<Point> newPoints;
-        int n = points.size();
-
-        for (int i = 0; i < n; i++) {
-            int next = (i + 1) % n;
-            double val1 = a * points[i].x + b * points[i].y + c;
-            double val2 = a * points[next].x + b * points[next].y + c;
-
-            if (val1 * val2 <= 0) {
-                double x = (points[i].x * abs(val2) + points[next].x * abs(val1)) / (abs(val1) + abs(val2));
-                double y = (points[i].y * abs(val2) + points[next].y * abs(val1)) / (abs(val1) + abs(val2));
-                newPoints.push_back(Point(x, y));
-            }
-
-            if (val2 == 0) {
-                newPoints.push_back(points[next]);
-            }
+    static bool comparePoints(const Point& p1, const Point& p2) 
+    {
+        if (p1.x == p2.x) 
+        {
+            return p1.y <= p2.y;
         }
-
-        points = newPoints;
+        return p1.x < p2.x;
     }
 
-
-
-    // Функция для проверки положения точки относительно прямой (лево/право)
-    int orientation(Point p1, Point p2, Point p3) {
-        int val = (p2.y - p1.y) * (p3.x - p2.x) - (p2.x - p1.x) * (p3.y - p2.y);
-        if (val == 0) return 0; // коллинеарны
-        return (val > 0) ? 1 : 2; // влево или вправо
+    static void sortPoints(ConvexPolygon& points) {
+        std::sort(points.vertices_.begin(), points.vertices_.end(), comparePoints);
     }
 
-    // Функция для нахождения точек пересечения отрезка с прямой
-    Point intersect(Point p1, Point p2, int a, int b, int c) {
-        Point intersectPoint;
-        intersectPoint.x = (b * (b * p1.x - a * p1.y) - a * c) / (a * a + b * b);
-        intersectPoint.y = (a * (-b * p1.x + a * p1.y) - b * c) / (a * a + b * b);
-        return intersectPoint;
-    }
-
-    // Функция для разделения многоугольника прямой
-    vector<vector<Point>> splitPolygon(vector<Point> polygon, Point p1, Point p2) {
-        vector<vector<Point>> result;
-        int n = polygon.size();
-        vector<int> side(n); // Массив для хранения положения точек относительно прямой
-
-        // Определяем положение каждой точки многоугольника относительно прямой
-        for (int i = 0; i < n; i++) {
-            side[i] = orientation(p1, p2, polygon[i]);
-        }
-
-        // Создаем два новых многоугольника
-        vector<Point> leftPolygon, rightPolygon;
-        for (int i = 0; i < n; i++) {
-            int next = (i + 1) % n;
-            if (side[i] == 1) leftPolygon.push_back(polygon[i]);
-            if (side[i] == 2) rightPolygon.push_back(polygon[i]);
-            if (side[i] == 0) {
-                leftPolygon.push_back(polygon[i]);
-                rightPolygon.push_back(polygon[i]);
-            }
-            if (side[i] * side[next] == 3) {
-                Point intersectPoint = intersect(polygon[i], polygon[next], p1.y - p2.y, p2.x - p1.x, p1.x * p2.y - p1.y * p2.x);
-                leftPolygon.push_back(intersectPoint);
-                rightPolygon.push_back(intersectPoint);
-            }
-        }
-
-        result.push_back(leftPolygon);
-        result.push_back(rightPolygon);
-
-        return result;
-    }
-
-
-
-    static double cross(const Point& a, const Point& b);
-    static bool isPointInPolygon(const Point& p, const ConvexPolygon& polygon);
+    bool searchPoint(std::vector<Point> points, Point p);
+    void intersect(double a, double b, double c);
+    double crossProduct(Point a, Point b, Point c);
+    void intersection(Point a, Point b);
+    void pointsAboveAndBelowLine(Point a, Point b);
+    bool isSegmentInside(Point a, Point b);
+    vector<vector<Point>> splitPolygon(const ConvexPolygon& poly1, const ConvexPolygon& poly2, const ConvexPolygon& poly3);
 
     ConvexPolygon& operator=(const ConvexPolygon& other);
-   // friend ConvexPolygon operator*(const ConvexPolygon& poly1, const ConvexPolygon& poly2);
 };
 
 class ConvexPolygon_Exception
@@ -147,52 +84,184 @@ ConvexPolygon::~ConvexPolygon() {}
 
 
 
-// Определитель для векторного произведения
-double ConvexPolygon::cross(const Point& a, const Point& b)
-{
-    return (a.x * b.y - a.y * b.x);
+bool ConvexPolygon::searchPoint(std::vector<Point> points, Point p) {
+    for (int i = 0; i < points.size(); i++) {
+        if (points[i].x == p.x && points[i].y == p.y) {
+            return true;
+        }
+    }
+    return false;
 }
 
-// Оператор присваивания
+double ConvexPolygon::crossProduct(Point a, Point b, Point c) 
+{
+    return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+
+
+void ConvexPolygon::intersect(double a, double b, double c) 
+{
+    vector<Point> newPoints;
+    int n = vertices_.size();
+
+    for (int i = 0; i < n; i++) 
+    {
+        int next = (i + 1) % n;
+        double val1 = a * vertices_[i].x + b * vertices_[i].y + c;
+        double val2 = a * vertices_[next].x + b * vertices_[next].y + c;
+
+        if (val1 * val2 <= 0) 
+        {
+            double x = (vertices_[i].x * abs(val2) + vertices_[next].x * abs(val1)) / (abs(val1) + abs(val2));
+            double y = (vertices_[i].y * abs(val2) + vertices_[next].y * abs(val1)) / (abs(val1) + abs(val2));
+            newPoints.push_back({x,y});
+        }
+
+        if (val2 == 0) {
+            newPoints.push_back(vertices_[next]);
+        }
+    }
+
+    cout << "Points of intersection:" << endl;
+    for (auto p : newPoints) {
+        cout << "(" << p.x << ", " << p.y << ")" << endl;
+    }
+}
+
+void ConvexPolygon::pointsAboveAndBelowLine(Point a, Point b) 
+{
+    vector<Point> above, below;
+    for (int i = 0; i < vertices_.size(); i++) {
+        if (crossProduct(a, b, vertices_[i]) > 0) {
+            above.push_back(vertices_[i]);
+        }
+        else {
+            below.push_back(vertices_[i]);
+        }
+    }
+    cout << "Points above the line:" << endl;
+    for (auto p : above) {
+        cout << "(" << p.x << ", " << p.y << ")" << endl;
+    }
+    cout << "Points below the line:" << endl;
+    for (auto p : below) {
+        cout << "(" << p.x << ", " << p.y << ")" << endl;
+    }
+}
+
+
+bool ConvexPolygon::isSegmentInside(Point a, Point b) {
+    for (int i = 0; i < vertices_.size(); i++) {
+        int j = (i + 1) % vertices_.size();
+        if (crossProduct(vertices_[i], vertices_[j], a) * crossProduct(vertices_[i], vertices_[j], b) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<vector<Point>> ConvexPolygon::splitPolygon(const ConvexPolygon& poly1, const ConvexPolygon& poly2, const ConvexPolygon& poly3)
+{
+    vector<vector<Point>> result;
+    vector<Point> inters = poly1.vertices_;
+    vector<Point> above = poly2.vertices_;
+    vector<Point> polygon = poly3.vertices_;
+    int k = 0;
+    int t = 5;
+
+        vector<Point> a;
+        for (int i = 0; i < polygon.size(); i++) {
+
+            if (searchPoint(above, polygon[i]) != searchPoint(above, polygon[i + 1]))
+            {
+                if (!(searchPoint(above, polygon[i])) && t != 5)
+                {
+                    a.push_back(polygon[i]);
+                }
+                if (searchPoint(above, polygon[i]) && t == 5)
+                {
+                     a.push_back(polygon[i]);
+                     a.push_back(inters[k]);
+                     result.push_back(a);
+                     cout << "LOL" << endl;
+                     cout << "Points of a:" << endl;
+                     for (auto p : a) {
+                         cout << "(" << p.x << ", " << p.y << ")" << endl;
+                     }
+                     a.clear();
+                     k++;
+                     continue;
+                }
+                else
+                {
+                    a.push_back(inters[k]);
+                    k++;
+                }
+            }
+
+            else if ((t != 5) && !(searchPoint(above, polygon[i])))
+            {
+                a.push_back(polygon[i]);
+            }
+            else if ((t == 5) && searchPoint(above, polygon[i]))
+            {
+                a.push_back(polygon[i]);
+            }
+
+         
+        }
+        cout << "Points of a2:" << endl;
+        for (auto p : a) {
+            cout << "(" << p.x << ", " << p.y << ")" << endl;
+        }
+        result.push_back(a);
+        a.clear();
+
+    cout << "Size of result:" << result.size() << endl;
+
+    return result;
+}
+
+
 ConvexPolygon& ConvexPolygon::operator=(const ConvexPolygon& other)
 {
-    // Копируем данные из другого объекта
     vertices_ = other.vertices_;
 
-    return *this; // Возвращаем ссылку на текущий объект
+    return *this;
 }
 
-// ConvexPolygon operator*(const ConvexPolygon& poly1, const ConvexPolygon& poly2)
+
+
+
+
+
 
 
 
 int main() {
-    ConvexPolygon poly({2, 0});
-    poly.addPoint(2, 0);
-    poly.addPoint(2, 3);
-    poly.addPoint(4, 2);
-    poly.addPoint(7, 4);
-    poly.addPoint(7, 0);
 
-    poly.intersect(-1, 5, -8); // Пример сечения прямой x - y - 2 = 0
+    ConvexPolygon polygon({ {2, 0}, {2, 4}, {5, 2}, {9, 3}, {11, 6}, {12, 9}, {13, 10}, {14, 9}, {13, 5}, {13, 0} });
 
-    vector<Point> polygon = { {2, 0}, {2, 3}, {4, 2}, {7, 4}, {7, 0} };
-    Point p1 = { 2, 2 };
-    Point p2 = { 7, 3 };
+    Point lineStart = { 2, 2 };
+    Point lineEnd = { 7, 5 };
 
-    ConvexPolygon poly2;
-    ConvexPolygon poly3;
+    cout << "Intersection points with the line:" << endl;
+    polygon.intersect(-1, 5, -8);
 
-    vector<vector<Point>> splitPolygons = splitPolygon(polygon, p1, p2);
+    cout << endl;
 
-    for (auto point : splitPolygons[0]) {
-        poly2.vertices_.addPoint(point.x, point.y);
-    }
+    cout << "Points above and below the line:" << endl;
+    polygon.pointsAboveAndBelowLine(lineStart, lineEnd);
 
-    for (auto point : splitPolygons[1]) {
-        poly3.vertices_.addPoint(point.x, point.y);
-    }
+    ConvexPolygon polygon2({ {2, 4}, {12, 9}, { 13, 10 } });
+    ConvexPolygon polygon3({ {2, 0}, {5, 2}, {9, 3}, {11, 6}, {14, 9}, {13, 5}, {13, 0} });
+    ConvexPolygon polygon1({ {2, 2}, {3.6, 2.9}, {11.6, 7.8}, {13.8, 9.2} });
+
+    polygon.splitPolygon(polygon1, polygon2, polygon);
+
+    cout << endl;
+
+    cout << "Is segment inside the polygon? " << (polygon.isSegmentInside({ 1, 1 }, { 3, 6 }) ? "Yes" : "No") << endl;
 
     return 0;
-
 }
