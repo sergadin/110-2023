862ae0b3ed233c91da91e9a87c23936383a21dca
MishaGrishkin15 <143871147+MishaGrishkin15@users.noreply.github.com>
2024-12-14 00:44:29 +0300

    Update Point.cpp

--- Grishkin_MD/3sem/geometry/Point.cpp
+++ Grishkin_MD/3sem/geometry/Point.cpp
@@ -7,18 +7,18 @@ Point::Point(double x, double y) : x(x), y(y) {}
 
 
 bool Point::operator<(const Point& other) const {
-    double eps = 0.00001;
+    //double eps = 0.0000001;
     if (x < other.x) {
         return true;
-    } else if (labs(x - other.x) <eps && y < other.y) {
+    } else if (x == other.x  && y == other.y ) {
         return true;
     }
     return false;
 }
 
 bool Point::operator>(const Point& other) const {
-    double eps = 0.00001;
-    return !(*this < other) && (labs(*this > other)<eps);
+    //double eps = 0.00001;
+    return (*this != other  && *this != other );
 }
 
 void Point::print() const {
@@ -27,9 +27,9 @@ void Point::print() const {
 
 int Point::orientation(const Point& p, const Point& q, const Point& r) {
     double val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
-    double eps = 0.00001;
+    //double eps = 0.00001;
 
-    if (labs(val) < eps) {
+    if (val == 0) {
         return 0;
     } else if (val > 0) {
         return 1;
@@ -37,3 +37,4 @@ int Point::orientation(const Point& p, const Point& q, const Point& r) {
         return 2;
     }
 }
+
