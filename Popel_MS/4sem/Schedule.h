453a557eafa99b2d4296f301645b704cf17611ea
maria-popel <mariia.popel@math.msu.ru>
2025-02-27 22:02:19 +0300

    Update h.file

--- Popel_MS/4sem/Schedule.h
+++ Popel_MS/4sem/Schedule.h
@@ -1,3 +1,4 @@
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -142,3 +143,106 @@ public:
   const std::vector<std::pair<Field, Order>>& getSortFields() const { return sort_fields_;}
 };
 
+
+
+
+
+/*
+ * Класс ячеек в базе данных.
+ * Переменные:
+ * time_ - время пары в расписании
+ * room_ - номер аудитории
+ * subject_name_ - название предмета
+ * teacher_ - ФИО преподавателя
+ * group_ - номер группы
+ */
+class Entry{
+  private: 
+    TimeHM time_;
+    int room_;
+    char subject_name_[128];
+    FullName teacher_;
+    int group_;
+    
+  public:
+    Entry(TimeHM time, int room, const char* subjName, FullName teacher, int group): time_(time), room_(room), teacher_(teacher), group_(group){
+      strncpy(subject_name_, subjName, sizeof(subject_name_));
+    }
+};
+
+
+/*
+ * Класс расписания.
+ * Переменные:
+ * schedule_ - само расписание - вектор ячеек
+ * teacherInd - индекс преподавателя
+ * groupInd - индекс группы
+ * roomInd - индекс аудитории
+ * timeInd - индекс времени пары
+ * subjectInd - индекс предмета
+ */
+class Schedule{
+  private: 
+    std::vector<Entry*> schedule_;
+    std::map<FullName, std::vector<int>> teacherInd;
+    std::map<int, std::vector<int>> groupInd;
+    std::map<int, std::vector<int>> roomInd;
+    std::map<TimeHM, std::vector<int>> timeInd;
+    std::map<char[128], std::vector<int>> subjectInd;
+    
+    //Функция, конструирующая и присваивающая индексы
+    void buildInd();
+    //Функция, получающая индекс по названию/номеру
+    std::vector<int> getCritInd(const std::string& crit);
+    //Функция, "пересекающая" множество индексов (для поиска по нескольким критериям)
+    std::vector<int> intesectInd(const std::vector<int>& ind1, const std::vector<int>& ind2);
+  public:
+    Schedule();
+    Schedule(FILE* fin);
+    ~Schedule();
+    //Функция добавления новой ячейки
+    void addEntry(Entry* entry);
+    //Функция удаления ячейки
+    void deleteEntry(const TimeHM& time, int room);
+    //Функция обновления уже существующей ячейки
+    void updateEntry(const TimeHM& time, int room, Entry* newEntry);
+    //Функция выбора ячейки для работы с ней
+    std::vector<Entry*> select(const std::vector<Cond>& crit);
+    //Функция выбора из уже имеющихся ячеек
+    std::vector<Entry*> reselect(const std::vector<Entry*>& selected, const std::vector<Cond>& crit);
+    //Функция вывода подходящих ячеек на экран
+    void print(const std::vector<Entry*>& entries/*, const std::vector<Field>& fields*/);
+    //Функция, сохраняющая в файл массив ячеек (базу данных)
+    void saveToFile(FILE* fout);
+    
+};
+
+/*
+ * Класс базы данных
+ * Переменная: schedule_ - расписание (массив ячеек).
+ */
+class DataBase{
+  private:
+    Schedule *schedule_;
+  public:
+    DataBase(FILE* fin);
+    ~DataBase();
+    //Функция, получающая в виде строчки результат обработки запроса
+    result startQuery(Query& query);
+    Schedule* getSchedule() const {return schedule_;}
+};
+
+/*
+ * Класс ошибок.
+ * Переменные: code_ - код ошибки, mess_ - причина ошибки;
+ */
+class Exception {
+  private:
+    int code_;
+    std::string mess_;
+  public:
+    Exception(int c, std::string m){
+      code_ = c;
+      mess_ = m;
+    }
+};
