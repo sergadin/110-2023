c861247f491a356a15ca111d9b78e6ff62e686b2
maria-popel <mariia.popel@math.msu.ru>
2025-03-17 00:24:57 +0300

    Something strange is happening in this file...

--- Popel_MS/4sem/Schedule.cpp
+++ Popel_MS/4sem/Schedule.cpp
@@ -1,6 +1,8 @@
 #include "Schedule.h"
 
-/*void result::addEntry(const Entry& ent) {
+std::list<Query::parser_fn> Query::parsers;
+
+void result::addEntry(const Entry& ent) {
     entry.push_back(ent);
 }
 
@@ -8,53 +10,26 @@ void result::addMessage(const std::string& mes) {
     message = mes;
 }
 
-void result::addStatistic(const std::string& key, const Value& value) {
-    statistics[key] = value;
-}
-
 void result::addError(const Exception& err) {
     error = err;
-    
-}*/
+}
 
 Cond::Cond(Field fld, BinOp optn, Value val) : field_(fld), operation_(optn), value_(val) {}
 
-Query::Query(const std::string &query) : query_(query) {
-    size_t pos = 0;
-    std::string token;
-    while ((pos = query_.find(' ')) != std::string::npos) {
-        token = query_.substr(0, pos);
-        if (token == "select") {
-            command_ = SELECT;
-        } else if (token == "reselect") {
-            command_ = RESELECT;
-        } else if (token == "insert") {
-            command_ = INSERT;
-        } else if (token == "update") {
-            command_ = UPDATE;
-        } else if (token == "delete") {
-            command_ = DELETE;
-        } else if (token == "print") {
-            command_ = PRINT;
-        }
-        query_.erase(0, pos + 1);
+Query::Query(const std::string &query) : query_(query){}
+
+Query * Query::do_parse(const std::string &query) {
+    for (auto p : Query::parsers) {
+        Query * q = new Query(query);
+        (*p)(*q, query); //Очень страшное нечто, но оно нужно для корректной проверки внутри парсинговых функций.
+        if (q != nullptr)
+            return q;
     }
-    if (!query_.empty()) {
-        token = query_;
-        if (token == "select") {
-            command_ = SELECT;
-        } else if (token == "reselect") {
-            command_ = RESELECT;
-        } else if (token == "insert") {
-            command_ = INSERT;
-        } else if (token == "update") {
-            command_ = UPDATE;
-        } else if (token == "delete") {
-            command_ = DELETE;
-        } else if (token == "print") {
-            command_ = PRINT;
-        }
-    }
+    throw Exception(1, "Unknown command\n");
+}
+
+void Query::register_parser(parser_fn p) {
+    Query::parsers.push_back(p);
 }
 
 SelectingQuery::SelectingQuery(const std::string &query) : Query(query) {}
@@ -70,6 +45,10 @@ void SelectingQuery::parseCond() {
         token = query_.substr(0, pos);
         if (token == "select") {
             command_ = SELECT;
+        }else if (token == "reselect") {
+            command_ = RESELECT;
+        }else if(token == "update"){
+          command_ = UPDATE;
         } else if (token == "end") {
             break;
         } else {
@@ -87,9 +66,6 @@ void SelectingQuery::parseCond() {
     }
 }
 
-void SelectingQuery::parseTriple(const std::string &frag) {
-    parseCondTriple(frag);
-}
 
 void SelectingQuery::parseCondTriple(const std::string &frag) {
     size_t pos = frag.find('=');
@@ -172,38 +148,130 @@ void SelectingQuery::parseCondTriple(const std::string &frag) {
                 condition_.emplace_back(SUBJNAME, EQ, value);
             }
         } else {
-            throw Exception(3, "Unknown field: " + field);
+            throw Exception(2, "Unknown field: " + field+ "\n");
         }
     } else {
-        throw Exception(3, "Invalid condition format: " + frag);
+        throw Exception(3, "Wrong condition format: " + frag+ "\n");
+    }
+}
+
+void SelectingQuery::parse(Query& q, const std::string &query) {
+    size_t pos = query.find(' ');
+    std::string token = query.substr(0, pos);
+    if (token == "select" || token == "reselect") {
+        SelectingQuery* sq = dynamic_cast<SelectingQuery*>(&q);
+        if (sq) {
+            sq->parse();
+        }
     }
 }
 
 AssigningQuery::AssigningQuery(const std::string &query): Query(query) {}
 
-void AssigningQuery::parse();
+void AssigningQuery::parse(){
+    parseKeyVal();
+}
 
-void AssigningQuery::parseKeyVal();
+void AssigningQuery::parseKeyVal(){
+    size_t pos = 0;
+    std::string token;
+    while ((pos = query_.find(' ')) != std::string::npos) {
+        token = query_.substr(0, pos);
+        if (token == "insert") {
+            command_ = INSERT;
+        }else if (token == "delete") {
+            command_ = DELETE;
+        }else if(token == "update"){
+          command_ = UPDATE;
+        }else if (token == "end") {
+            break;
+        } else {
+            parseKeyValTriple(token);
+        }
+        query_.erase(0, pos + 1);
+    }
+    if (!query_.empty()) {
+        token = query_;
+        if (token == "end") {
+            return;
+        } else {
+            parseKeyValTriple(token);
+        }
+    }
+}
 
-void AssigningQuery::parseTriple(const std::string &frag);
+void AssigningQuery::parseKeyValTriple(const std::string &frag){
+    size_t pos = frag.find('=');
+    if (pos != std::string::npos) {
+        std::string field = frag.substr(0, pos);
+        std::string value = frag.substr(pos + 1);
+        if (field == "teacher") {
+            values_.emplace_back(TEACHER, value);
+        } else if (field == "group") {
+            values_.emplace_back(GROUP, std::stoi(value));
+        } else if (field == "room") {
+            values_.emplace_back(ROOM, std::stoi(value));
+        } else if (field == "time") {
+            TimeHM time;
+            sscanf(value.c_str(), "%d:%d-%d:%d", &time.startHour, &time.startMinute, &time.endHour, &time.endMinute);
+            values_.emplace_back(TIME, time);
+        } else if (field == "subject") {
+            values_.emplace_back(SUBJNAME, value);
+        } else {
+            throw Exception(2, "Unknown field: " + field + "\n");
+        }
+    } else {
+        throw Exception(3, "Wrong key-value format: " + frag + "\n");
+    }
+}
 
-void AssigningQuery::parseKeyValTriple(const std::string &frag);
+void AssigningQuery::parse(Query& q, const std::string &query) {
+    size_t pos = query.find(' ');
+    std::string token = query.substr(0, pos);
+    if (token == "insert" || token == "delete") {
+        AssigningQuery* aq = dynamic_cast<AssigningQuery*>(&q);
+        if (aq) {
+            aq->parse();
+        }
+    }
+}
 
 UpdateQuery::UpdateQuery(const std::string &query) : SelectingQuery(query), AssigningQuery(query) {}
 
-void UpdateQuery::parseCond();
+void UpdateQuery::parseCond(){
+    SelectingQuery::parseCond();
+}
 
-void UpdateQuery::parseKeyVal();
+void UpdateQuery::parseKeyVal(){
+    AssigningQuery::parseKeyVal();
+}
 
-void UpdateQuery::parseCondTriple(const std::string &frag);
+void UpdateQuery::parseCondTriple(const std::string &frag)) {
+    SelectingQuery::parseCondTriple(frag);
+}
 
-void UpdateQuery::parseKeyValTriple(const std::string &frag);
+void UpdateQuery::parseKeyValTriple(const std::string &frag){
+    AssigningQuery::parseKeyValTriple(frag);
+}
+
+void UpdateQuery::parse(Query& q, const std::string &query) {
+    size_t pos = query.find(' ');
+    std::string token = query.substr(0, pos);
+    if (token == "update") {
+        UpdateQuery* up = dynamic_cast<UpdateQuery*>(&q);
+        if (up) {
+            up->parseCond();
+            up->parseKeyVal();
+        }
+    }
+}
 
 PrintQuery::PrintQuery(const std::string &query) : Query(query) {}
 
 void PrintQuery::parse();
 
 void PrintQuery::parseTriple(const std::string &frag);
+void PrintQuery::parse(Query& q, const std::string &query);
 
 Schedule::Schedule() {}
 
@@ -225,7 +293,7 @@ Schedule::Schedule(FILE* fin) {
             addEntry(entry);
         }
     }else {
-        throw Exception(5, "Unable to open file");
+        throw Exception(4, "File isn't open\n");
     }
 }
 
@@ -237,15 +305,19 @@ Schedule::~Schedule() {
 
 void Schedule::addEntry(Entry* entry) {
     for (const auto& exstEntry : schedule_) {
-        /*if (exstEntry->room == entry->room && checkTimeMatch(exstEntry->time, entry->time)) {
-            throw Exception(2, "Time match in the same room");//Мб еще что-то аналогичное для схожих пересечений
+        /*if (exstEntry->room == entry->room && checkTimeCross(exstEntry->time, entry->time)) {
+            throw Exception(5, "Time match in the same room");//Мб еще что-то аналогичное для схожих пересечений
         }*/
     }
     schedule_.push_back(entry);
 }
 
 void Schedule::deleteEntry(const TimeHM& time, int room) {
-    ...
+    auto it = std::remove_if(schedule_.begin(), schedule_.end(), [&](Entry* entry) {
+        return entry->time.startHour == time.startHour && entry->time.startMinute == time.startMinute &&
+               entry->time.endHour == time.endHour && entry->time.endMinute == time.endMinute && entry->room == room;
+    });
+    schedule_.erase(it, schedule_.end());
 }
 
 void Schedule::updateEntry(const TimeHM& time, int room, Entry* newEntry) {
@@ -335,7 +407,7 @@ void Schedule::saveToFile(FILE* fout) {
 }
 
 
-/*bool Schedule::checkTimeMatch(const TimeHM& time1, const TimeHM& time2) {
+/*bool Schedule::checkTimeCross(const TimeHM& time1, const TimeHM& time2) {
     return !(time1.endHour < time2.startHour || time1.endHour == time2.startHour && time1.endMinute <= time2.startMinute ||
              time2.endHour < time1.startHour || time2.endHour == time1.startHour && time2.endMinute <= time1.startMinute);
 }*/
@@ -404,4 +476,7 @@ result DataBase::startQuery(std::string& query) {
     return res;
 }
 
-
+Query::register_parser(SelectingQuery::parse);
+Query::register_parser(AssigningQuery::parse);
+Query::register_parser(UpdateQuery::parse);
+Query::register_parser(PrintQuery::parse);
