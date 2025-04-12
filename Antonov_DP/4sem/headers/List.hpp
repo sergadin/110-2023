ee901887d58c7b155f6e38c88ca7bf6817ad7eb1
Антонов Дмитрий Павлович <Antonov_DP@ns139.class2.math.msu.su>
2025-04-12 14:17:44 +0300

    Некоторые добавления в струкуры.

--- Antonov_DP/4sem/headers/List.hpp
+++ Antonov_DP/4sem/headers/List.hpp
@@ -2,5 +2,11 @@
 
 
 class List{
-	std::vector<student> students;
-}
+	std::vector<student> students_;
+public:
+	List();
+	~List();
+	int Add(student &st); //Добавляет студента. OK - успешно, N_OK - в противном случае.
+	int Delete(double rating); //Удаляет студента по рейтингу. OK - успешно, N_OK - в противном случае.
+	student pull(double rating); //Берёт данные студента по рейтингу.
+};
