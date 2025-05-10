6444a400bb5726b549e487f505dc510f098d439b
Антонов Дмитрий Павлович <Antonov_DP@ns156.class2.math.msu.su>
2025-05-10 14:45:31 +0300

    что-то

--- Antonov_DP/4sem/headers/Hash.cpp
+++ Antonov_DP/4sem/headers/Hash.cpp
@@ -2,7 +2,25 @@
 
 
 unsigned int Hash_func(int gr){
-	
+	unsigned int n = (unsigned int) ((31 * ((int) (gr / 100)) + 11 * (gr % 100))%max_size);
+	return n;
+}
+
+
+void Hash::Add_group(int &gr){
+	int st = hash_func(gr);
+	int i = 0;
+	while (hash_func[st].val_){
+		if (i == max_size) { break;}
+		st++;
+		i++;
+	}
+	if ( i == max_size){
+		std::cout << 1 << "\n";
+	}
+	else{
+		hash_func[st].val_ = gr;
+	}
 }
 
 
--- Antonov_DP/4sem/headers/Hash.hpp
+++ Antonov_DP/4sem/headers/Hash.hpp
@@ -9,13 +9,19 @@ union h_object{
 };
 
 class Hash {
-	std::vector<h_object> hash_;
+	h_object* hash_;
 	size_t max_size;
-	unsigned int Hash_func(int gr);
+	friend unsigned int Hash_func(int gr);
 public:
+	Hash(size_t max_size = 100){
+		hash_ = new h_object[max_size];
+	}
+	~Hash(){
+		delete[] hash_;
+	}
 	void Add_group(int &gr);
 	void Add_student(student &st);
 	void Delete_group(int &gr);
 	void Delete_student(int &gr, char name[64]);
-	vector<student> Give_group(int &gr);
+	std::vector<student> Give_group(int &gr);
 };
