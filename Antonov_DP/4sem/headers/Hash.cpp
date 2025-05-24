da7c14eba527221a15710cf84c86f98b8b3afff7
Антонов Дмитрий Павлович <Antonov_DP@ns128.class2.math.msu.su>
2025-05-24 14:01:16 +0300

    sd

--- Antonov_DP/4sem/headers/Hash.cpp
+++ Antonov_DP/4sem/headers/Hash.cpp
@@ -16,7 +16,7 @@ void Hash::Add_group(int &gr){
 		st = (st + 1)%max_size;
 		i++;
 	}
-	if ( i == max_size){
+	if (i == max_size){
 		std::cout << 1 << "\n";
 	}
 	else{
@@ -35,15 +35,31 @@ void Hash::Delete_group(int &gr){
                 i++;
         }
 	std::cout << 2 << "\n";
-        if ( i == max_size){
+        if (i == max_size){
                 std::cout << 1 << "\n";
         }
         else{
                 hash_[st].val_ = 0;
-		hash_[st].list_root_->~List();
 		std::cout << 2 << "\n";
-		hash_[st].tree_root_->~TreeNode();
         }
 	std::cout << 2 << "\n";
         std::cout << hash_[st].val_ << "\n";
+}
+
+void Hash::Add_student(student &st){
+	int st = this->Hash_func(gr);
+	int i = 0;
+        std::cout << 2 << "\n";
+        while ((hash_[st].val_ != gr) && hash_[st].val_){
+                if (i == max_size) { break;}
+                st = (st + 1)%max_size;
+                i++;
+        }
+	if (i == max_size){
+		std::cout << 1 << "\n";
+	}
+	else if(hash_[st].val_ == 0) {
+		hash_[st].val_ = gr;
+		hash_[st].tree_root->add(&st);
+	}
 }
